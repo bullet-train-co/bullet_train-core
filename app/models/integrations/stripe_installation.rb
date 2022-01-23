7a40e899056025de69a3a00eeb1138eb9e3d4bb9
Andrew Culver <andrew.culver@gmail.com>
2022-01-22 17:08:49 -0800

    Making it easier to override models.

--- app/models/integrations/stripe_installation.rb
+++ app/models/integrations/stripe_installation.rb
@@ -1,26 +1,7 @@
 class Integrations::StripeInstallation < ApplicationRecord
-  # 🚅 add concerns above.
-
-  belongs_to :team
-  belongs_to :oauth_stripe_account, class_name: "Oauth::StripeAccount"
-  # 🚅 add belongs_to associations above.
-
-  # 🚅 add has_many associations above.
-
-  # 🚅 add has_one associations above.
-
-  # 🚅 add scopes above.
-
-  validates :name, presence: true
-  # 🚅 add validations above.
-
-  # 🚅 add callbacks above.
-
-  # 🚅 add delegations above.
+  include Integrations::StripeInstallations::Base
 
   def process_webhook(webhook)
-    raise "You need to create a `app/models/integrations/stripe_installation.rb` file in your application that does a `Integrations::StripeInstallation.class_eval do ... end` and defines a `def process_webhook(webhook)` method."
+    raise "You should implement a `Integrations::StripeInstallation` model in your application that has `include Integrations::StripeInstallations::Base` and implements a `def process_webhook(webhook)` method."
   end
-
-  # 🚅 add methods above.
 end
