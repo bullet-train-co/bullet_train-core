cc6ec78c4e8c63d8ad184ee61168887dea7fac85
Andrew Culver <andrew.culver@gmail.com>
2022-01-30 22:37:19 -0800

    Generating engine.

---
+++ lib/bullet_train/scope_questions.rb
@@ -0,0 +1,8 @@
+require "bullet_train/scope_questions/version"
+require "bullet_train/scope_questions/engine"
+
+module BulletTrain
+  module ScopeQuestions
+    # Your code goes here...
+  end
+end
---
+++ lib/bullet_train/scope_questions/version.rb
@@ -0,0 +1,5 @@
+module BulletTrain
+  module ScopeQuestions
+    VERSION = "0.1.0"
+  end
+end
