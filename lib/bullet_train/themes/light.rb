a375b4042e440150f06d39aa2211032f0aa9e779
Andrew Culver <andrew.culver@gmail.com>
2022-02-15 22:54:09 -0800

    v1.0.3

--- lib/bullet_train/themes/light.rb
+++ lib/bullet_train/themes/light.rb
@@ -1,10 +1,15 @@
 require "bullet_train/themes/light/version"
 require "bullet_train/themes/light/engine"
-require "bullet_train/themes/light/theme"
+require "bullet_train/themes/tailwind_css"
 
 module BulletTrain
   module Themes
     module Light
+      class Theme < BulletTrain::Themes::TailwindCss::Theme
+        def directory_order
+          ['light'] + super
+        end
+      end
     end
   end
 end
--- lib/bullet_train/themes/light/version.rb
+++ lib/bullet_train/themes/light/version.rb
@@ -1,7 +1,7 @@
 module BulletTrain
   module Themes
     module Light
-      VERSION = "1.0.2"
+      VERSION = "1.0.3"
     end
   end
 end
