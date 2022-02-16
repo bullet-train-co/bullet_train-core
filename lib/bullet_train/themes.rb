298e191b6e026ccb9682ef0971ccf265f920653b
Andrew Culver <andrew.culver@gmail.com>
2022-02-15 22:19:51 -0800

    Include base theme.

--- lib/bullet_train/themes.rb
+++ lib/bullet_train/themes.rb
@@ -1,8 +1,17 @@
 require "bullet_train/themes/version"
 require "bullet_train/themes/engine"
+# require "bullet_train/themes/base/theme"
 
 module BulletTrain
   module Themes
     mattr_accessor :themes, default: {}
+
+    module Base
+      class Theme
+        def directory_order
+          ['base']
+        end
+      end
+    end
   end
 end
