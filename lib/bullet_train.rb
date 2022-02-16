f97ed30469dda4eb8c4577404e45b2251b668760
Andrew Culver <andrew.culver@gmail.com>
2022-02-15 22:55:25 -0800

    v1.0.17

--- lib/bullet_train.rb
+++ lib/bullet_train.rb
@@ -1,6 +1,12 @@
 require "bullet_train/version"
 require "bullet_train/engine"
 
+require "bullet_train/fields"
+require "bullet_train/roles"
+require "bullet_train/super_load_and_authorize_resource"
+require "bullet_train/has_uuid"
+require "bullet_train/scope_validator"
+
 module BulletTrain
   mattr_accessor :routing_concerns, default: []
 end
