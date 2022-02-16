1ed35597eebff29cc3e1922729239d01012a3e62
Andrew Culver <andrew.culver@gmail.com>
2022-02-15 22:54:59 -0800

    v1.0.5

--- lib/bullet_train/themes/tailwind_css.rb
+++ lib/bullet_train/themes/tailwind_css.rb
@@ -1,10 +1,15 @@
 require "bullet_train/themes/tailwind_css/version"
 require "bullet_train/themes/tailwind_css/engine"
-require "bullet_train/themes/tailwind_css/theme"
+require "bullet_train/themes"
 
 module BulletTrain
   module Themes
     module TailwindCss
+      class Theme < BulletTrain::Themes::Base::Theme
+        def directory_order
+          ['tailwind_css'] + super
+        end
+      end
     end
   end
 end
