bc1f27028871c6894ede8894aa1c9b22a5f7a92b
Andrew Culver <andrew.culver@gmail.com>
2022-02-15 21:45:16 -0800

    Add API support.

--- lib/bullet_train/outgoing_webhooks/engine.rb
+++ lib/bullet_train/outgoing_webhooks/engine.rb
@@ -1,6 +1,13 @@
 module BulletTrain
   module OutgoingWebhooks
     class Engine < ::Rails::Engine
+      initializer "bullet_train.outgoing_webhooks.register_api_endpoints" do |app|
+        if BulletTrain::Api
+          BulletTrain::Api.endpoints << "Api::V1::Webhooks::Outgoing::EndpointsEndpoint"
+          BulletTrain::Api.endpoints << "Api::V1::Webhooks::Outgoing::DeliveriesEndpoint"
+          BulletTrain::Api.endpoints << "Api::V1::Webhooks::Outgoing::DeliveryAttemptsEndpoint"
+        end
+      end
     end
   end
 end
