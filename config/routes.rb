a4efa7b0654cc7c3c08e5e1e6f288aadf8c20f6f
Andrew Culver <andrew.culver@gmail.com>
2022-02-15 21:43:50 -0800

    Add web support.

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,18 @@
 Rails.application.routes.draw do
+  namespace :account do
+    shallow do
+      resources :teams do
+        namespace :webhooks do
+          namespace :outgoing do
+            resources :events
+            resources :endpoints do
+              resources :deliveries, only: [:index, :show] do
+                resources :delivery_attempts, only: [:index, :show]
+              end
+            end
+          end
+        end
+      end
+    end
+  end
 end
