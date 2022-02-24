76d1764ae0a7371c98a37f9817667429b46d039f
Andrew Culver <andrew.culver@gmail.com>
2022-02-24 17:26:07 -0500

    v1.0.2

--- lib/bullet_train/fields/engine.rb
+++ lib/bullet_train/fields/engine.rb
@@ -1,6 +1,9 @@
 module BulletTrain
   module Fields
     class Engine < ::Rails::Engine
+      initializer "bullet_train.fields" do |app|
+        BulletTrain.linked_gems << "bullet_train-fields"
+      end
     end
   end
 end
