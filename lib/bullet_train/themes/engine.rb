c3482c143c8882c4a43d3a1fb1cf9bb98d74b7bb
Andrew Culver <andrew.culver@gmail.com>
2022-02-24 17:20:42 -0500

    v1.0.5

--- lib/bullet_train/themes/engine.rb
+++ lib/bullet_train/themes/engine.rb
@@ -1,6 +1,9 @@
 module BulletTrain
   module Themes
     class Engine < ::Rails::Engine
+      initializer "bullet_train.themes.register" do |app|
+        BulletTrain.linked_gems << "bullet_train-themes"
+      end
     end
   end
 end
