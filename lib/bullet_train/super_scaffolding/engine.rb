f3492d265b2bf08692b3319b8a28bcb17d9d511b
Andrew Culver <andrew.culver@gmail.com>
2022-03-03 09:39:35 -0800

    Improve support for older Bullet Train applications.

--- lib/bullet_train/super_scaffolding/engine.rb
+++ lib/bullet_train/super_scaffolding/engine.rb
@@ -11,14 +11,17 @@ module BulletTrain
       end
 
       initializer "bullet_train.super_scaffolding.templates.register_api_endpoints" do |app|
-        if BulletTrain::Api
+        if defined?(BulletTrain::Api)
           BulletTrain::Api.endpoints << "Api::V1::Scaffolding::AbsolutelyAbstract::CreativeConceptsEndpoint"
           BulletTrain::Api.endpoints << "Api::V1::Scaffolding::CompletelyConcrete::TangibleThingsEndpoint"
         end
       end
 
       initializer "bullet_train.super_scaffolding.register" do |app|
-        BulletTrain.linked_gems << "bullet_train-super_scaffolding"
+        # Older versions of Bullet Train have a `BulletTrain` module, but it doesn't have `linked_gems`.
+        if BulletTrain.respond_to?(:linked_gems)
+          BulletTrain.linked_gems << "bullet_train-super_scaffolding"
+        end
       end
     end
   end
