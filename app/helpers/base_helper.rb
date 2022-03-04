fe4723d8dbc76b3c1bc105aac126d6794d61bcec
Andrew Culver <andrew.culver@gmail.com>
2022-03-04 09:04:16 -0800

    Import library support.

---
+++ app/helpers/base_helper.rb
@@ -0,0 +1,6 @@
+module BaseHelper
+  # TODO This is for the billing package to override, but I feel like there has got to be a better way to do this.
+  def hide_team_resource_menus?
+    false
+  end
+end
