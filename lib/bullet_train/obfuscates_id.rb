3b6688a2d26f9e92cf9a6029e7a1b1feafb0c2e6
Andrew Culver <andrew.culver@gmail.com>
2022-03-03 18:44:37 -0800

    v1.0.2

--- lib/bullet_train/obfuscates_id.rb
+++ lib/bullet_train/obfuscates_id.rb
@@ -1,6 +1,8 @@
 require "bullet_train/obfuscates_id/version"
 require "bullet_train/obfuscates_id/engine"
 
+require "hashids"
+
 module BulletTrain
   module ObfuscatesId
     # Your code goes here...
