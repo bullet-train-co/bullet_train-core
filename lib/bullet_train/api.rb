bd46a945c7a863e2703271026b937715b1b86ee9
Andrew Culver <andrew.culver@gmail.com>
2022-03-03 18:41:36 -0800

    Fixing requires.

--- lib/bullet_train/api.rb
+++ lib/bullet_train/api.rb
@@ -3,14 +3,14 @@ require "bullet_train/api/engine"
 
 require "grape"
 require "grape-cancan"
-require "grape-jsonapi"
+require "grape_jsonapi"
 require "grape-swagger"
 require "grape_on_rails_routes"
 # require "wine_bouncer"
 require "kaminari"
 require "api-pagination"
-require "rack-cors"
-require "jsonapi-serializer"
+require "rack/cors"
+require "jsonapi/serializer"
 require "doorkeeper"
 
 module BulletTrain
