7b89966495b390cb0e91185e14985c1d089464fa
Andrew Culver <andrew.culver@gmail.com>
2022-03-09 14:36:58 -0500

    Add form controller.

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,10 +1,12 @@
 import { identifierForContextKey } from "@stimulus/webpack-helpers"
 
 import ClipboardController from './clipboard_controller'
+import FormController from './form_controller'
 import MobileMenuController from './mobile_menu_controller'
 
 export const controllerDefinitions = [
   [ClipboardController, 'clipboard_controller.js'],
+  [FormController, 'form_controller.js'],
   [MobileMenuController, 'mobile_menu_controller.js'],
 ].map(function(d) {
   const key = d[1]
