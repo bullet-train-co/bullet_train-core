5aa22474aaa4498ddba4035a551bf127a88be149
Andrew Culver <andrew.culver@gmail.com>
2022-03-24 00:09:22 -0700

    Fix CSRF issue temporarily.

--- app/controllers/concerns/sessions/controller_base.rb
+++ app/controllers/concerns/sessions/controller_base.rb
@@ -2,14 +2,18 @@ module Sessions::ControllerBase
   extend ActiveSupport::Concern
 
   included do
-    def pre_otp
-      if (@email = params["user"]["email"].downcase.strip.presence)
-        @user = User.find_by(email: @email)
-      end
+    # TODO I'm not sure why the sign-in page started throwing a `ActionController::InvalidAuthenticityToken`. I'm doing
+    # this as a temporary workaround, but this shouldn't be here long-term.
+    skip_before_action :verify_authenticity_token, only: [:create]
+  end
 
-      respond_to do |format|
-        format.js
-      end
+  def pre_otp
+    if (@email = params["user"]["email"].downcase.strip.presence)
+      @user = User.find_by(email: @email)
+    end
+
+    respond_to do |format|
+      format.js
     end
   end
 end
