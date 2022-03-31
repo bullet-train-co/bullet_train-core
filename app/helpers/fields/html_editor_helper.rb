9d1b0b67c5fd78f98e0077e528e4d0045f5aef5f
Andrew Culver <andrew.culver@gmail.com>
2022-03-31 12:58:38 -0700

    Consolidate HTML sanitization.

--- app/helpers/fields/html_editor_helper.rb
+++ app/helpers/fields/html_editor_helper.rb
@@ -1,8 +1,16 @@
 module Fields::HtmlEditorHelper
   TEMPORARY_REPLACEMENT = "https://temp.bullettrain.co/"
+
   def html_sanitize(string)
     return string unless string
+    # TODO this is a hack to get around the fact that rails doesn't allow us to add any acceptable protocols.
+    string = string.gsub("bullettrain://", TEMPORARY_REPLACEMENT)
     string = sanitize(string, tags: %w[div br strong em b i del a h1 blockquote pre ul ol li], attributes: %w[href])
+    # given the limited scope of what we're doing here, this string replace should work.
+    # it should also use a lot less memory than nokogiri.
+    string = string.gsub(/<a href="#{TEMPORARY_REPLACEMENT}(.*?)\/.*?">(.*?)<\/a>/o, "<span class=\"tribute-reference tribute-\\1-reference\">\\2</span>").html_safe
+
+    # Also, while we're at it ...
     links_target_blank(string).html_safe
   end
 
--- app/helpers/fields/trix_editor_helper.rb
+++ app/helpers/fields/trix_editor_helper.rb
@@ -1,17 +1,13 @@
 module Fields::TrixEditorHelper
   TEMPORARY_REPLACEMENT = "https://temp.bullettrain.co/"
-  def trix_sanitize(string)
-    return string unless string
-    # TODO this is a hack to get around the fact that rails doesn't allow us to add any acceptable protocols.
-    string = string.gsub("bullettrain://", TEMPORARY_REPLACEMENT)
-    string = sanitize(string, tags: %w[div br strong em del a h1 blockquote pre ul ol li], attributes: %w[href])
-    # given the limited scope of what we're doing here, this string replace should work.
-    # it should also use a lot less memory than nokogiri.
-    string = string.gsub(/<a href="#{TEMPORARY_REPLACEMENT}(.*?)\/.*?">(.*?)<\/a>/o, "<span class=\"tribute-reference tribute-\\1-reference\">\\2</span>").html_safe
-    trix_content(string)
+
+  # TODO We should migrate away from this, but I think we'll need to update Super Scaffolding.
+  def trix_sanitize(html)
+    html_sanitize(html)
   end
 
+  # TODO Confirm there are no references to this anywhere (in other repositories) and remove it.
   def trix_content(body)
-    links_target_blank(body).html_safe
+    html_sanitize(string)
   end
 end
