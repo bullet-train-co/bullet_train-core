2f8ce1d317ce0d8b093e4a570061df5ae2e73926
João Roberto <2695893+jorbs@users.noreply.github.com>
2022-05-16 02:05:27 -0300

    Add strength indicator for password field (#15)

---
+++ app/javascript/controllers/fields/password_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "stimulus";
+import zxcvbn from "zxcvbn";
+
+export default class extends Controller {
+  static targets = ["strengthIndicator"];
+
+  estimateStrength(e) {
+    const result = zxcvbn(e.target.value);
+
+    if (result.feedback) {
+      this.strengthIndicatorTarget.innerText = result.feedback.warning;
+      this.strengthIndicatorTarget.classList.remove("hidden");
+    } else {
+      this.strengthIndicatorTarget.classList.add("hidden");
+    }
+  }
+}
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,6 +5,7 @@ import CloudinaryImageController from './fields/cloudinary_image_controller'
 import ColorPickerController from './fields/color_picker_controller'
 import DateController from './fields/date_controller'
 import FileFieldController from './fields/file_field_controller'
+import PasswordController from './fields/password_controller'
 import PhoneController from './fields/phone_controller'
 import SuperSelectController from './fields/super_select_controller'
 
@@ -14,8 +15,9 @@ export const controllerDefinitions = [
   [ColorPickerController, 'fields/color_picker_controller.js'],
   [DateController, 'fields/date_controller.js'],
   [FileFieldController, 'fields/file_field_controller.js'],
+  [PasswordController, 'fields/password_controller.js'],
   [PhoneController, 'fields/phone_controller.js'],
-  [SuperSelectController, 'fields/super_select_controller.js']
+  [SuperSelectController, 'fields/super_select_controller.js'],
 ].map(function(d) {
   const key = d[1]
   const controller = d[0]
@@ -31,6 +33,7 @@ export {
   ColorPickerController,
   DateController,
   FileFieldController,
+  PasswordController,
   PhoneController,
-  SuperSelectController
+  SuperSelectController,
 }
