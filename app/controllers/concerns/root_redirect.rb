1014474a7efc7f0b3bb13d9d2094ff8df098ac14
Andrew Culver <andrew.culver@gmail.com>
2022-06-10 15:13:09 -0400

    Workaround to get Devise working with Turbo. (#58)

--- app/controllers/concerns/root_redirect.rb
+++ app/controllers/concerns/root_redirect.rb
@@ -2,10 +2,6 @@ module RootRedirect
   extend ActiveSupport::Concern
 
   def index
-    if ENV["MARKETING_SITE_URL"]
-      redirect_to ENV["MARKETING_SITE_URL"]
-    else
-      redirect_to new_user_session_path
-    end
+    redirect_to ENV["MARKETING_SITE_URL"] || new_user_session_path
   end
 end
