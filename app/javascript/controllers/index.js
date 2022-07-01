119f062630a41deeac149ef3e2c9f8f937e51757
Andrew Culver <andrew.culver@gmail.com>
2022-06-30 20:00:32 -0700

    Add some controllers temporarily.

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,10 +1,14 @@
 import { identifierForContextKey } from "@hotwired/stimulus-webpack-helpers"
 
+import BulkActionFormController from './bulk_action_form_controller'
+import BulkActionsController from './bulk_actions_controller'
 import ClipboardController from './clipboard_controller'
 import FormController from './form_controller'
 import MobileMenuController from './mobile_menu_controller'
 
 export const controllerDefinitions = [
+  [BulkActionFormController, 'bulk_action_form_controller.js'],
+  [BulkActionsController, 'bulk_actions_controller.js'],
   [ClipboardController, 'clipboard_controller.js'],
   [FormController, 'form_controller.js'],
   [MobileMenuController, 'mobile_menu_controller.js'],
