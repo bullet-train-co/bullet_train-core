e3c1336c60b343b685ab0fd2e4809b6da6a019f3
Gabriel Zayas <g-zayas@hotmail.com>
2022-07-26 05:54:43 +0900

    Fix logout logic user details page (#48)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,14 @@
 class SessionsController < Devise::SessionsController
   include Sessions::ControllerBase
+
+  def destroy
+    if params.include?(:onboard_logout)
+      signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+      set_flash_message! :notice, :signed_out if signed_out
+      yield if block_given?
+      redirect_to root_path
+    else
+      super
+    end
+  end
 end
