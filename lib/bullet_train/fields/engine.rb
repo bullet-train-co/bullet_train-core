a5be337da6f2986903369e2cb4737aa952022cb5
Gabriel Zayas <g-zayas@hotmail.com>
2022-07-26 06:58:39 +0900

    Prep repo for running minitest tests (#19)

--- lib/bullet_train/fields/engine.rb
+++ lib/bullet_train/fields/engine.rb
@@ -2,7 +2,10 @@ module BulletTrain
   module Fields
     class Engine < ::Rails::Engine
       initializer "bullet_train.fields" do |app|
-        BulletTrain.linked_gems << "bullet_train-fields"
+        # Older versions of Bullet Train have a `BulletTrain` module, but it doesn't have `linked_gems`.
+        if BulletTrain.respond_to?(:linked_gems)
+          BulletTrain.linked_gems << "bullet_train-fields"
+        end
       end
     end
   end
