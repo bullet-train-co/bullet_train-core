e12c8c88c85a5e8556ffb35cffdfb4336f739e62
Andrew Culver <andrew.culver@gmail.com>
2022-07-25 20:01:06 -0700

    Fixing CI. (#9)

--- lib/bullet_train/sortable/engine.rb
+++ lib/bullet_train/sortable/engine.rb
@@ -2,7 +2,7 @@ module BulletTrain
   module Sortable
     class Engine < ::Rails::Engine
       initializer "bullet_train.sortable.register_routing_concerns" do |app|
-        BulletTrain.routing_concerns << Proc.new do
+        BulletTrain.routing_concerns << proc do
           concern :sortable do
             collection do
               post :reorder
