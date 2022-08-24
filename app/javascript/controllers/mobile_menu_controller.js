7c11a17fe241a94ffbc3e5e795ebcbfb7d9bd3d8
Pascal Laliberte <pascal@hey.com>
2022-08-23 21:47:02 -0400

    Fix mobile menu breaking after repeated reveal/hide: Change mobile_menu_controller.js (#79)

--- app/javascript/controllers/mobile_menu_controller.js
+++ app/javascript/controllers/mobile_menu_controller.js
@@ -1,24 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
+import { enter, leave } from "el-transition"
 
 export default class extends Controller {
-  static targets = [ "wrapper"]
-  static classes = [ "hidden" ] // necessary because stimulus-reveal will mess with the [hidden] attribute on the wrapper
-  static values = {
-    showEventName: String,
-    hideEventName: String,
-  }
-
-  toggle() {
-    const eventName = this.isWrapperHidden? this.showEventNameValue: this.hideEventNameValue
-    if (this.isWrapperHidden) {
-      this.showWrapper()
-    }
-    
-    this.wrapperTarget.dispatchEvent(new CustomEvent(eventName))
+  static targets = [ "wrapper", "revealable"]
+  static classes = [ "hidden" ] // necessary because we're always hiding the mobile menu on larger screens and this is the class used for only mobile screen sizes
+  
+  open() {
+    this.showWrapper()
+    this.revealableTargets.forEach(revealableTarget => {
+      enter(revealableTarget)
+    })
   }
   
-  get isWrapperHidden() {
-    return this.wrapperTarget.classList.contains(this.hiddenClass)
+  close() {
+    Promise.all(
+      this.revealableTargets.map(revealableTarget => {
+        return leave(revealableTarget)
+      })
+    ).then(() => {
+      this.hideWrapper()
+    })
+    
   }
   
   showWrapper() {
