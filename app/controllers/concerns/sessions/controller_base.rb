41a7cf1c006308d4e0bd9df2acbed9c5bab1e217
Gabriel Zayas <g-zayas@hotmail.com>
2022-08-24 10:41:55 +0900

    Remove token verification workaround for signing in (#96)

--- app/controllers/concerns/sessions/controller_base.rb
+++ app/controllers/concerns/sessions/controller_base.rb
@@ -1,12 +1,6 @@
 module Sessions::ControllerBase
   extend ActiveSupport::Concern
 
-  included do
-    # TODO I'm not sure why the sign-in page started throwing a `ActionController::InvalidAuthenticityToken`. I'm doing
-    # this as a temporary workaround, but this shouldn't be here long-term.
-    skip_before_action :verify_authenticity_token, only: [:create]
-  end
-
   def pre_otp
     if (@email = params["user"]["email"].downcase.strip.presence)
       @user = User.find_by(email: @email)
