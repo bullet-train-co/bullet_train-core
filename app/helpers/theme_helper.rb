a137471c30103b1f564a279dfab81aa742999c63
Gabriel Zayas <g-zayas@hotmail.com>
2022-08-24 10:26:15 +0900

    Monkey patch to ensure custom devise views are used (#15)

--- app/helpers/theme_helper.rb
+++ app/helpers/theme_helper.rb
@@ -1,3 +1,39 @@
+# Our custom devise views are housed in `bullet_train-base`, but they're overwritten
+# by devise's standard views if the devise gem is declared after `bullet_train`.
+# To ensure we use our custom views, we temporarily unregister the original devise
+# views path from the FileSystemResolver, add our custom path, and find the views that way.
+module ActionView
+  class LookupContext
+    module ViewPaths
+      def find(name, prefixes = [], partial = false, keys = [], options = {})
+        name, prefixes = normalize_name(name, prefixes)
+        details, details_key = detail_args_for(options)
+
+        devise_view = false
+        prefixes.each do |prefix|
+          if prefix.match?(/devise/)
+            devise_view = true
+            break
+          end
+        end
+
+        resolver = if name == "devise" || devise_view
+          original_devise_view_path = BulletTrain::Themes::Light.original_devise_path
+          bullet_train_resolver = @view_paths.paths.reject do |resolver|
+            resolver.path.match?(original_devise_view_path)
+          end
+          PathSet.new(bullet_train_resolver)
+        else
+          @view_paths
+        end
+
+        resolver.find(name, prefixes, partial, details, details_key, keys)
+      end
+      alias_method :find_template, :find
+    end
+  end
+end
+
 module ThemeHelper
   # TODO Do we want this to be configurable by downstream applications?
   INVOCATION_PATTERNS = [
