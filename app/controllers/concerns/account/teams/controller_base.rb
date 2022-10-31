95c47730117fe232b7de36ada5b97f9eb3304e8a
Gabriel Zayas <g-zayas@hotmail.com>
2022-10-31 12:02:21 +0900

    Get unit tests working on CircleCI (#128)
    * Only include strong parameters when module is present
    
    * Move strong parameters method back to general controller
    
    * Use class from starter repo to check for strong params

--- app/controllers/concerns/account/teams/controller_base.rb
+++ app/controllers/concerns/account/teams/controller_base.rb
@@ -21,7 +21,9 @@ module Account::Teams::ControllerBase
 
     private
 
-    include strong_parameters_from_api
+    if defined?(Api::V1::ApplicationController)
+      include strong_parameters_from_api
+    end
   end
 
   # GET /teams
