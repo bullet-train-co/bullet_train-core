dab0f786938ed0d38c3511acea571f22509ff1f3
Gabriel Zayas <g-zayas@hotmail.com>
2022-12-15 09:32:09 +0900

    Move `render_pagination` to `bullet_train-api` (#125)
    Co-authored-by: Andrew Culver <andrew.culver@gmail.com>

--- app/helpers/concerns/helpers/base.rb
+++ app/helpers/concerns/helpers/base.rb
@@ -6,13 +6,4 @@ module Helpers::Base
     # This scope has an order if the SQL changes when we remove any order clause.
     scope.to_sql != scope.reorder("").to_sql
   end
-
-  # TODO This should really be in the API package and included from there.
-  if defined?(BulletTrain::Api)
-    def render_pagination(json)
-      if @pagy
-        json.has_more @pagy.has_more
-      end
-    end
-  end
 end
