043607db6253281b6b1c5f0493c4f15f0102d75f
Jeremy Green <jagthedrummer@gmail.com>
2022-12-14 18:38:38 -0600

    Disable Turbo on the sign in form when user_return_to points to an oauth path (#102)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,15 @@
 class SessionsController < Devise::SessionsController
   include Sessions::ControllerBase
 
+  # If user_return_to points to an oauth path we disable Turbo on the sign in form.
+  # This makes it work when we need to redirect to external sites and/or custom protocols.
+  # With Turbo enabled the browser will block those redirects with a CORS error.
+  # https://github.com/bullet-train-co/bullet_train/issues/384
+  def user_return_to_is_oauth
+    session["user_return_to"]&.match(/^\/oauth/)
+  end
+  helper_method :user_return_to_is_oauth
+
   def destroy
     if params.include?(:onboard_logout)
       signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
