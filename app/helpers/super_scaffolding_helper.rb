49ecc93d05cd7f6b0e2c628309a9206f460f53dc
Gabriel Zayas <g-zayas@hotmail.com>
2022-12-15 09:48:52 +0900

    Remove equal sign causing duplicate code, add comment to helper (#86)

--- app/helpers/super_scaffolding_helper.rb
+++ app/helpers/super_scaffolding_helper.rb
@@ -1,10 +1,13 @@
 module SuperScaffoldingHelper
   unless defined?(BulletTrain::ActionModels)
+    # action_model_select_controller is originally a method
+    # in the Action Models package, but we can't simply
+    # remove this method from Super Scaffolded index partials
+    # when the package isn't present. Since we keep
+    # action_model_select_controller in our views, we need
+    # this method so we don't get a NoMethodError.
     def action_model_select_controller
-      # TODO I don't know why if I just call `yield` I get duplicate content on the page.
-      tag.div do
-        yield
-      end
+      yield
     end
   end
 end
