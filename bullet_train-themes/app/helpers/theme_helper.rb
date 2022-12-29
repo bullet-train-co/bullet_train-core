8282e71b538ef00a225b8feefafff99e7b4517e3
Kasper Timm Hansen <kaspth@gmail.com>
2022-12-30 00:43:15 +0100

    Fix Devise + BulletTrain engine loading order issue without runtime monkey patches (#33)

--- bullet_train-themes/app/helpers/theme_helper.rb
+++ bullet_train-themes/app/helpers/theme_helper.rb
@@ -1,39 +1,3 @@
-# Our custom devise views are housed in `bullet_train-base`, but they're overwritten
-# by devise's standard views if the devise gem is declared after `bullet_train`.
-# To ensure we use our custom views, we temporarily unregister the original devise
-# views path from the FileSystemResolver, add our custom path, and find the views that way.
-module ActionView
-  class LookupContext
-    module ViewPaths
-      def find(name, prefixes = [], partial = false, keys = [], options = {})
-        name, prefixes = normalize_name(name, prefixes)
-        details, details_key = detail_args_for(options)
-
-        devise_view = false
-        prefixes.each do |prefix|
-          if prefix.match?(/devise/)
-            devise_view = true
-            break
-          end
-        end
-
-        resolver = if name == "devise" || devise_view
-          original_devise_view_path = BulletTrain::Themes::Light.original_devise_path
-          bullet_train_resolver = @view_paths.paths.reject do |resolver|
-            resolver.path.match?(original_devise_view_path)
-          end
-          PathSet.new(bullet_train_resolver)
-        else
-          @view_paths
-        end
-
-        resolver.find(name, prefixes, partial, details, details_key, keys)
-      end
-      alias_method :find_template, :find
-    end
-  end
-end
-
 module ThemeHelper
   def current_theme_object
     @current_theme_object ||= "BulletTrain::Themes::#{current_theme.to_s.classify}::Theme".constantize.new
