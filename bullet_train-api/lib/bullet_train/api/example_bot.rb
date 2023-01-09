cdc1cac2cd2c29b77069807c24402d7cef6b7455
Yuri Sidorov <yurisidorov@evrone.com>
2023-01-09 17:30:47 +0100

    Added ExampleBot

---
+++ bullet_train-api/lib/bullet_train/api/example_bot.rb
@@ -0,0 +1,36 @@
+require "factory_bot"
+require "active_support/inflector/methods"
+
+module BulletTrain
+  module Api
+    module ExampleBot
+      SUFFIX = "-bullet_train-api-example"
+
+      def define(&block)
+        DSL.run(block)
+      end
+
+      def example(model)
+        object = FactoryBot.build("#{model}#{SUFFIX}", id: 1)
+        object.created_at = Time.now if object.respond_to?(:created_at?)
+        object.updated_at = Time.now if object.respond_to?(:updated_at?)
+        object
+      end
+
+      class DSL
+        def example(name, **options, &block)
+          options[:class] ||= name.to_s.pluralize.classify
+          name = "#{name}#{BulletTrain::Api::ExampleBot::SUFFIX}"
+
+          FactoryBot.define { factory(name, **options, &block) }
+        end
+
+        def self.run(block)
+          new.instance_eval(&block)
+        end
+      end
+    end
+
+    extend ExampleBot
+  end
+end
