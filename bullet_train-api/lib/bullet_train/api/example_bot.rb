9e376d9ff60a2ecc314c83b2f53902b6b82c026b
Yuri Sidorov <yurisidorov@evrone.com>
2023-01-17 19:43:19 +0100

    Parameter examples

--- bullet_train-api/lib/bullet_train/api/example_bot.rb
+++ bullet_train-api/lib/bullet_train/api/example_bot.rb
@@ -48,7 +48,7 @@ module BulletTrain
         #   end
         def example(name, **options, &block)
           options[:class] ||= name.to_s.pluralize.classify
-          name = BulletTrain::Api.send(:example_name, name, @version)
+          name = BulletTrain::Api.send(:_example_name, name, @version)
 
           FactoryBot.define { factory(name, **options, &block) }
         end
@@ -89,7 +89,7 @@ module BulletTrain
       # Example:
       #   BulletTrain::Api.example(:user)
       def example(model, **options)
-        object = FactoryBot.build(example_name(model, options.delete(:version)), **options)
+        object = FactoryBot.build(_example_name(model, options.delete(:version)), **options)
         object.id ||= 1
         object.created_at = Time.now if object.respond_to?(:created_at?)
         object.updated_at = Time.now if object.respond_to?(:updated_at?)
@@ -101,7 +101,7 @@ module BulletTrain
       # Example:
       #   BulletTrain::Api.example_list(:user, 10)
       def example_list(model, quantity, **options)
-        objects = FactoryBot.build_list(example_name(model, options.delete(:version)), quantity, **options)
+        objects = FactoryBot.build_list(_example_name(model, options.delete(:version)), quantity, **options)
         objects.map.with_index do |object, index|
           object.id ||= index + 1
           object.created_at = Time.now if object.respond_to?(:created_at?)
@@ -112,51 +112,81 @@ module BulletTrain
 
       %i[get_examples get_example post_examples post_parameters put_example put_parameters patch_example patch_parameters].each do |method|
         define_method(method) do |model, **options|
-          path_examples(method.to_s, model, **options)
+          _path_examples(method.to_s, model, **options)
         end
       end
 
       private
 
       # Generates example name from model, suffix and version
-      def example_name(model, version = nil)
+      def _example_name(model, version = nil)
         version ||= "v1"
         "#{model}#{SUFFIX}#{version}"
       end
 
-      def path_examples(method, model, **options)
+      def _path_examples(method, model, **options)
+        version = options.delete(:version) || "v1"
+
         case method.split("_").first
         when "get"
-          version = options.delete(:version) || "v1"
           count = (options.delete(:count) || method == "get_examples") ? 2 : 1
-
-          if count > 1
-            values = BulletTrain::Api.example_list(model, count, version: version)
-            class_name = values.first.class.name
-            var_name = class_name.demodulize.underscore.pluralize
-          else
-            values = BulletTrain::Api.example(model, version: version)
-            class_name = values.class.name
-            var_name = class_name.demodulize.underscore
-          end
-
-          template = (method == "get_examples") ? "index" : "show"
+          template, class_name, var_name, values = _set_values(method, model, version, count)
         else
           case method.split("_").last
           when "example", "examples"
-            return path_examples("get_example", model, **options)
+            template, class_name, var_name, values = _set_values("get_example", model, version)
           else
+            template, class_name, var_name, values = _set_values("get_example", model, version)
+
+            if has_strong_parameters?("::Api::#{version.upcase}::#{class_name.pluralize}Controller".constantize)
+              strong_params_module = "::Api::#{version.upcase}::#{class_name.pluralize}Controller::StrongParameters".constantize
+              strong_parameter_keys = BulletTrain::Api::StrongParametersReporter.new(class_name.constantize, strong_params_module).report
+              if strong_parameter_keys.last.is_a?(Hash)
+                strong_parameter_keys += strong_parameter_keys.pop.keys
+              end
+
+              output = _json_output(template, version, class_name, var_name, values)
+
+              parameters_output = JSON.parse(output)
+              parameters_output&.select! { |key| strong_parameter_keys.include?(key.to_sym) }
+
+              return indent(parameters_output.to_yaml.delete_prefix("---\n"), 6).html_safe
+            end
             return nil
           end
         end
 
+        _yaml_output(template, version, class_name, var_name, values)
+      end
+
+      def _set_values(method, model, version, count = 1)
+        if count > 1
+          values = BulletTrain::Api.example_list(model, count, version: version)
+          class_name = values.first.class.name
+          var_name = class_name.demodulize.underscore.pluralize
+        else
+          values = BulletTrain::Api.example(model, version: version)
+          class_name = values.class.name
+          var_name = class_name.demodulize.underscore
+        end
+
+        template = (method == "get_examples") ? "index" : "show"
+
+        [template, class_name, var_name, values]
+      end
+
+      def _json_output(template, version, class_name, var_name, values)
+        ActionController::Base.render(
+          template: "api/#{version}/#{class_name.underscore.pluralize}/#{template}",
+          assigns: {"#{var_name}": values},
+          formats: :json
+        )
+      end
+
+      def _yaml_output(template, version, class_name, var_name, values)
         indent(
           JSON.parse(
-            ActionController::Base.render(
-              template: "api/#{version}/#{class_name.underscore.pluralize}/#{template}",
-              assigns: {"#{var_name}": values},
-              formats: :json
-            )
+            _json_output(template, version, class_name, var_name, values)
           ).to_yaml
            .delete_prefix("---\n"), 7
         ).html_safe
@@ -181,7 +211,7 @@ module FactoryBot
     #   end
     def example(name, **options, &block)
       version = options.delete(:version) || BulletTrain::Api.send(:example_version, caller(1..1).first)
-      name = BulletTrain::Api.send(:example_name, name, version)
+      name = BulletTrain::Api.send(:_example_name, name, version)
       @child_factories << [name, options, block]
     end
   end
@@ -202,7 +232,7 @@ module FactoryBot
         super(name, false)
         @options = options.dup
         @overrides = options.extract_options!
-        @overrides[:example] = BulletTrain::Api.send(:example_name, @overrides[:example], @overrides[:version]) if @overrides[:example]
+        @overrides[:example] = BulletTrain::Api.send(:_example_name, @overrides[:example], @overrides[:version]) if @overrides[:example]
         @factory_name = @overrides.delete(:example) || @overrides.delete(:factory) || name
         @traits = options
       end
--- bullet_train-super_scaffolding/app/views/api/v1/open_api/examples/scaffolding/absolutely_abstract/creative_concepts.rb
+++ bullet_train-super_scaffolding/app/views/api/v1/open_api/examples/scaffolding/absolutely_abstract/creative_concepts.rb
@@ -1,7 +1,7 @@
 BulletTrain::Api.define do
   example :scaffolding_absolutely_abstract_creative_concept, class: "Scaffolding::AbsolutelyAbstract::CreativeConcept" do
-    team
-    name { "Example MyString" }
-    description { "MyText" }
+    team { BulletTrain::Api.example(:team, scaffolding_absolutely_abstract_creative_concepts: [self.instance]) }
+    name { "Example Creative Concept" }
+    description { "Example Text" }
   end
 end
