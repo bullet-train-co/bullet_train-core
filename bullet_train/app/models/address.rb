8f1fbdfc98b80f1584f468fca443687be41f083a
Pascal Laliberté <pascal@hey.com>
2023-01-23 18:40:47 +0000

    add Address model, Addresses::Base concern

---
+++ bullet_train/app/models/address.rb
@@ -0,0 +1,24 @@
+class Team < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  include Addresses::Base
+  # 🚅 add concerns above.
+
+  # 🚅 add belongs_to associations above.
+
+  # 🚅 add has_many associations above.
+
+  # 🚅 add oauth providers above.
+
+  # 🚅 add has_one associations above.
+
+  # 🚅 add scopes above.
+
+  # 🚅 add validations above.
+
+  # 🚅 add callbacks above.
+
+  # 🚅 add delegations above.
+
+  # 🚅 add methods above.
+end
