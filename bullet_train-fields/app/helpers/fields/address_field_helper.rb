9fe0201409f6a08289dd1e95330e9e803db6d1cb
Pascal Laliberté <pascal@hey.com>
2023-01-24 15:54:47 +0000

    adapt region name and postal code field labels per country

--- bullet_train-fields/app/helpers/fields/address_field_helper.rb
+++ bullet_train-fields/app/helpers/fields/address_field_helper.rb
@@ -6,4 +6,21 @@ module Fields::AddressFieldHelper
   def populate_region_options(address_form)
     Addresses::Region.where(country_id: address_form.object.country_id).map { |region| [region.name, region.id] }
   end
+  
+  def admin_division_label_for(address_form)
+    country = Addresses::Country.find(address_form.object.country_id)
+    admin_divisions_key = country.admin_divisions.presence || "default"
+    path = [:addresses, :fields, :admin_divisions, admin_divisions_key]
+    t(path.compact.join("."))
+  end
+  
+  def postal_code_label_for(address_form)
+    key = if address_form.object.country_id == 233
+      :zip_code 
+    else
+      :postal_code
+    end
+    path = [:addresses, :fields, key, :label]
+    t(path.compact.join("."))
+  end
 end
