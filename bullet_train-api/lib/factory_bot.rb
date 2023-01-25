af4d9223343d2b26177481776abb09da549ffa35
Yuri Sidorov <403994+newstler@users.noreply.github.com>
2023-01-25 16:18:17 +0100

    Works for 1 example

--- bullet_train-api/lib/factory_bot.rb
+++
@@ -1,9 +0,0 @@
-module FactoryBot
-  def self.create_example(name, *traits_and_overrides)
-    ActiveRecord::Base.transaction do
-      instance = create(name, *traits_and_overrides)
-      # you can use instance.dup here or OpenStruct.new(instance.attributes)
-      raise ActiveRecord::Rollback
-    end
-  end
-end
