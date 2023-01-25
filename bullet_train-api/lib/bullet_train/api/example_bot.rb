42c1aba994b78f3f15a6e12d588f5ec11a09eee2
Yuri Sidorov <403994+newstler@users.noreply.github.com>
2023-01-25 16:18:17 +0100

    Works for 1 example

--- bullet_train-api/lib/bullet_train/api/example_bot.rb
+++ bullet_train-api/lib/bullet_train/api/example_bot.rb
@@ -1,57 +1,41 @@
-# module FactoryBot
-#   def self.create_example(name, *traits_and_overrides)
-#     ActiveRecord::Base.transaction do
-#       instance = create(name, *traits_and_overrides)
-#       # you can use instance.dup here or OpenStruct.new(instance.attributes)
-#       raise ActiveRecord::Rollback
-#     end
-#   end
-# end
-#
-# # module BulletTrain
-# #   module Api
-# #     module ExampleBot
-# #       def example(model, **options)
-# #         factory = "#{model}_example"
-# #
-# #         object = nil
-# #         id = nil
-# #
-# #         ActiveRecord::Base.transaction do
-# #           # begin
-# #
-# #           object = FactoryBot.create(factory, **options)
-# #           id = object.id
-# #
-# #           puts ">>>ID1 #{object.id}"
-# #           puts ">>>REFLECTIONS #{object.reflections}"
-# #
-# #           raise ActiveRecord::Rollback
-# #
-# #           puts ">>>ID2 #{object.id}"
-# #
-# #           # rescue ActiveRecord::Rollback
-# #
-# #           # end
-# #         end
-# #
-# #         puts ">>>ID3 #{object.id}"
-# #
-# #         ActiveRecord::Base.connection.reset_pk_sequence!(model.to_s.pluralize)
-# #
-# #         object.id = id
-# #
-# #         puts ">>>ID4 #{object.id}"
-# #         object
-# #       end
-# #
-# #       def example_list(model, quantity, **options)
-# #
-# #       end
-# #     end
-# #
-# #     extend ExampleBot
-# #   end
-# # end
-# #
-# #
+module BulletTrain
+  module Api
+    module ExampleBot
+      def example(model, **options)
+        factory = "#{model}_example"
+        tables_to_reset = [model.to_s.pluralize]
+        object = nil
+
+        ActiveRecord::Base.transaction do
+          instance = FactoryBot.create(factory, **options)
+          object = instance.clone
+
+          instance.class.reflections.each do |name, reflection|
+            if reflection.macro == :has_many
+              associations = instance.send(name).map { |association| association.clone }
+              object.send("#{name}=", associations)
+              tables_to_reset << name
+            elsif reflection.macro == :belongs_to
+              object.send("#{name}=", instance.send(name).clone)
+              tables_to_reset << name.pluralize
+            end
+          end
+
+          raise ActiveRecord::Rollback
+        end
+
+        tables_to_reset.each do |name|
+          ActiveRecord::Base.connection.reset_pk_sequence!(name)
+        end
+
+        object
+      end
+
+      def example_list(model, quantity, **options)
+
+      end
+    end
+
+    extend ExampleBot
+  end
+end
