9a11b086272d60b31efdf8313db5d043c3cc0fe7
Pascal Laliberté <pascal@hey.com>
2023-01-26 22:29:27 +0000

    address_field: restore previous field values after dependent fields refreshed

--- bullet_train-fields/app/javascript/controllers/refresh_fields_controller.js
+++ bullet_train-fields/app/javascript/controllers/refresh_fields_controller.js
@@ -1,11 +1,26 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = [ "field" ]
+  static values = {
+    valuesStore: {
+      type: Object,
+      default: {}
+    }
+  }
+  
   updateFrameFromDependentSuperSelectValue(event) {
     const dependentSuperSelect = event?.detail?.event?.detail?.event?.target // original super select jQuery event
     
+    this.storeFieldValues()
+    
     const frame = this.element
     frame.src = this.constructNewUrlUpdatingField(dependentSuperSelect.name, dependentSuperSelect.value)
+  }
+  
+  finishFrameUpdate(event) {
+    if (event.target !== this.element) { return }
+    this.restoreFieldValues()
   }
   
   constructNewUrlUpdatingField(fieldName, fieldValue) {
@@ -13,5 +28,23 @@ export default class extends Controller {
     url.searchParams.set(fieldName, fieldValue)
     
     return url.href
+  }
+  
+  storeFieldValues() {
+    this.fieldTargets.forEach(field => {
+      let storeUpdate = {}
+      storeUpdate[field.name] = field.value
+      this.valuesStoreValue = Object.assign(this.valuesStoreValue, storeUpdate)
+    })
+  }
+  
+  restoreFieldValues() {
+    this.fieldTargets.forEach(field => {
+      const storedValue = this.valuesStoreValue[field.name]
+      if (storedValue === undefined) { return }
+      field.value = storedValue
+    })
+    
+    this.valuesStoreValue = {}
   }
 }
