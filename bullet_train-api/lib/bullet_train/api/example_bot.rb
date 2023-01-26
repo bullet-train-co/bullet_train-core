92e697ad8534de2957e2a4c3e0b0f56874c64bab
Yuri Sidorov <403994+newstler@users.noreply.github.com>
2023-01-26 12:57:27 +0100

    Cleanup & added path methods

--- bullet_train-api/lib/bullet_train/api/example_bot.rb
+++ bullet_train-api/lib/bullet_train/api/example_bot.rb
@@ -1,3 +1,5 @@
+require_relative "../../../app/helpers/api/open_api_helper"
+
 module FactoryBot
   module ExampleBot
     attr_accessor :tables_to_reset
@@ -39,6 +41,12 @@ module FactoryBot
       objects
     end
 
+    %i[get_examples get_example post_examples post_parameters put_example put_parameters patch_example patch_parameters].each do |method|
+      define_method(method) do |model, **options|
+        _path_examples(method.to_s, model, **options)
+      end
+    end
+
     private
 
     def reset_tables!
@@ -55,13 +63,79 @@ module FactoryBot
           associations = instance.send(name).map { |association| association.clone }
           clone.send("#{name}=", associations)
           @tables_to_reset << name
-        elsif reflection.macro == :belongs_to
+        elsif %i[belongs_to has_one].include?(reflection.macro)
           clone.send("#{name}=", instance.send(name).clone)
           @tables_to_reset << name.pluralize
         end
       end
 
       clone
+    end
+
+    include ::Api::OpenApiHelper
+    def _path_examples(method, model, **options)
+      version = options.delete(:version) || "v1"
+
+      case method.split("_").first
+      when "get"
+        count = (options.delete(:count) || method == "get_examples") ? 2 : 1
+        template, class_name, var_name, values = _set_values(method, model, count)
+      else
+        template, class_name, var_name, values = _set_values("get_example", model)
+
+        unless %w[example examples].include?(method.split("_").last)
+          if has_strong_parameters?("::Api::#{version.upcase}::#{class_name.pluralize}Controller".constantize)
+            strong_params_module = "::Api::#{version.upcase}::#{class_name.pluralize}Controller::StrongParameters".constantize
+            strong_parameter_keys = BulletTrain::Api::StrongParametersReporter.new(class_name.constantize, strong_params_module).report
+            if strong_parameter_keys.last.is_a?(Hash)
+              strong_parameter_keys += strong_parameter_keys.pop.keys
+            end
+
+            output = _json_output(template, version, class_name, var_name, values)
+
+            parameters_output = JSON.parse(output)
+            parameters_output&.select! { |key| strong_parameter_keys.include?(key.to_sym) }
+
+            return indent(parameters_output.to_yaml.delete_prefix("---\n"), 6).html_safe
+          end
+          return nil
+        end
+      end
+
+      _yaml_output(template, version, class_name, var_name, values)
+    end
+
+    def _set_values(method, model, count = 1)
+      if count > 1
+        values = FactoryBot.example_list(model, count)
+        class_name = values.first.class.name
+        var_name = class_name.demodulize.underscore.pluralize
+      else
+        values = FactoryBot.example(model)
+        class_name = values.class.name
+        var_name = class_name.demodulize.underscore
+      end
+
+      template = (method == "get_examples") ? "index" : "show"
+
+      [template, class_name, var_name, values]
+    end
+
+    def _json_output(template, version, class_name, var_name, values)
+      ActionController::Base.render(
+        template: "api/#{version}/#{class_name.underscore.pluralize}/#{template}",
+        assigns: {"#{var_name}": values},
+        formats: :json
+      )
+    end
+
+    def _yaml_output(template, version, class_name, var_name, values)
+      indent(
+        JSON.parse(
+          _json_output(template, version, class_name, var_name, values)
+        ).to_yaml
+            .delete_prefix("---\n"), 7
+      ).html_safe
     end
   end
 
--- bullet_train-super_scaffolding/app/views/api/v1/open_api/examples/scaffolding/absolutely_abstract/creative_concepts.rb
+++
@@ -1,7 +0,0 @@
-BulletTrain::Api.define do
-  example :scaffolding_absolutely_abstract_creative_concept, class: "Scaffolding::AbsolutelyAbstract::CreativeConcept" do
-    team { BulletTrain::Api.example(:team, scaffolding_absolutely_abstract_creative_concepts: [self.instance]) }
-    name { "Example Creative Concept" }
-    description { "Example Text" }
-  end
-end
