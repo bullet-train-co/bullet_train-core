fdfb3a0ff38a5c1347579aecf7963b67287bde23
Pascal Laliberté <pascal@hey.com>
2023-01-26 23:07:17 +0000

    refresh_fields: toggle loading styling, disable fields while loading

--- bullet_train-fields/app/javascript/controllers/refresh_fields_controller.js
+++ bullet_train-fields/app/javascript/controllers/refresh_fields_controller.js
@@ -3,16 +3,18 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "field" ]
   static values = {
-    valuesStore: {
-      type: Object,
-      default: {}
-    }
+    valuesStore: { type: Object, default: {} },
+    loading: { type: Boolean, default: false }
   }
+  static classes = [ "loading" ]
   
   updateFrameFromDependentSuperSelectValue(event) {
     const dependentSuperSelect = event?.detail?.event?.detail?.event?.target // original super select jQuery event
     
     this.storeFieldValues()
+    
+    this.loadingValue = true
+    this.disableRefreshingFields()
     
     const frame = this.element
     frame.src = this.constructNewUrlUpdatingField(dependentSuperSelect.name, dependentSuperSelect.value)
@@ -20,7 +22,9 @@ export default class extends Controller {
   
   finishFrameUpdate(event) {
     if (event.target !== this.element) { return }
+    
     this.restoreFieldValues()
+    this.loadingValue = false
   }
   
   constructNewUrlUpdatingField(fieldName, fieldValue) {
@@ -28,6 +32,15 @@ export default class extends Controller {
     url.searchParams.set(fieldName, fieldValue)
     
     return url.href
+  }
+  
+  disableRefreshingFields() {
+    this.fieldTargets.forEach(field => field.disabled = true )
+  }
+  
+  loadingValueChanged() {
+    if (!this.hasLoadingClass) { return }
+    this.element.classList.toggle(...this.loadingClasses, this.loadingValue)
   }
   
   storeFieldValues() {
