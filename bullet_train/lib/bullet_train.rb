51b369dee671833ccd699444fafc6ede0c11d700
Kasper Timm Hansen <kaspth@gmail.com>
2023-02-14 05:12:12 +0100

    Ensure we include the Light theme

--- bullet_train/lib/bullet_train.rb
+++ bullet_train/lib/bullet_train.rb
@@ -29,7 +29,6 @@ require "premailer/rails"
 require "figaro"
 require "valid_email"
 require "commonmarker"
-require "showcase" if Rails.env.development?
 require "rouge" if Rails.env.development?
 require "extended_email_reply_parser"
 require "pagy"
