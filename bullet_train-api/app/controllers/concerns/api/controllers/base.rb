f20d4caacafd010c39a54c3ae6b8a6c07f492ca5
Gabriel Zayas <g-zayas@hotmail.com>
2023-02-20 18:20:13 +0900

    Fixing Standard Ruby

--- bullet_train-api/app/controllers/concerns/api/controllers/base.rb
+++ bullet_train-api/app/controllers/concerns/api/controllers/base.rb
@@ -75,10 +75,10 @@ module Api::Controllers::Base
     # TODO Make this logic configurable so that downstream developers can write different methods for this column getting updated.
     if doorkeeper_token
       begin
-        current_time = unless @current_user.time_zone.nil?
-          ActiveSupport::TimeZone.all.find {|time_zone| time_zone.name == @current_user.time_zone }.now
-        else
+        current_time = if @current_user.time_zone.nil?
           Time.zone.now
+        else
+          ActiveSupport::TimeZone.all.find { |time_zone| time_zone.name == @current_user.time_zone }.now
         end
         doorkeeper_token.update(last_used_at: current_time)
       rescue ActiveRecord::StatementInvalid => _
