4b74c3692b867508b9c24ddb033431fca5704708
Kasper Timm Hansen <kaspth@gmail.com>
2023-03-06 21:50:05 +0100

    Doesn't look like we're using Active Storage

--- bullet_train-roles/test/dummy/config/application.rb
+++ bullet_train-roles/test/dummy/config/application.rb
@@ -5,7 +5,6 @@ require_relative "boot"
 require "rails"
 require "active_model/railtie"
 require "active_record/railtie"
-require "active_storage/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
