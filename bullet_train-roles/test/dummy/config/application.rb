6868e4c8614d2a4f9f3e9be998951d021089a313
Kasper Timm Hansen <kaspth@gmail.com>
2023-03-07 00:51:34 +0100

    Add back active_storage/engine require

--- bullet_train-roles/test/dummy/config/application.rb
+++ bullet_train-roles/test/dummy/config/application.rb
@@ -5,6 +5,7 @@ require_relative "boot"
 require "rails"
 require "active_model/railtie"
 require "active_record/railtie"
+require "active_storage/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
