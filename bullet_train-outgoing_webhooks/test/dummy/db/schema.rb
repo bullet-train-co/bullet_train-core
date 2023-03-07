e6d98056088b8c024cabd3162e8630b0e4881b5a
Kasper Timm Hansen <kaspth@gmail.com>
2023-03-07 03:58:55 +0100

    Start setting up with postgres for jsonb support

---
+++ bullet_train-outgoing_webhooks/test/dummy/db/schema.rb
@@ -0,0 +1,62 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.0].define(version: 2022_11_30_063357) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "webhooks_outgoing_deliveries", force: :cascade do |t|
+    t.integer "endpoint_id"
+    t.integer "event_id"
+    t.text "endpoint_url"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.datetime "delivered_at", precision: nil
+  end
+
+  create_table "webhooks_outgoing_delivery_attempts", force: :cascade do |t|
+    t.integer "delivery_id"
+    t.integer "response_code"
+    t.text "response_body"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.text "response_message"
+    t.text "error_message"
+    t.integer "attempt_number"
+  end
+
+  create_table "webhooks_outgoing_endpoints", force: :cascade do |t|
+    t.bigint "team_id"
+    t.text "url"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.string "name"
+    t.jsonb "event_type_ids", default: []
+    t.bigint "scaffolding_absolutely_abstract_creative_concept_id"
+    t.index ["scaffolding_absolutely_abstract_creative_concept_id"], name: "index_endpoints_on_abstract_creative_concept_id"
+    t.index ["team_id"], name: "index_webhooks_outgoing_endpoints_on_team_id"
+  end
+
+  create_table "webhooks_outgoing_events", force: :cascade do |t|
+    t.integer "subject_id"
+    t.string "subject_type"
+    t.jsonb "data"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.bigint "team_id"
+    t.string "uuid"
+    t.jsonb "payload"
+    t.string "event_type_id"
+    t.index ["team_id"], name: "index_webhooks_outgoing_events_on_team_id"
+  end
+
+end
--- bullet_train-outgoing_webhooks/test/setup/active_record.rb
+++ bullet_train-outgoing_webhooks/test/setup/active_record.rb
@@ -1,7 +1,5 @@
 require "active_record"
 
-ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ":memory:")
-
 # Set so Active Record casts created_at/updated_at to ActiveSupport::TimeWithZone.
 # (Mathces Rails apps' default, but its assigned via Active Record's railtie.)
 Time.zone = "UTC"
