c6c385f93fb30744ddb4f861ddf5a8977f90f0cc
Kasper Timm Hansen <kaspth@gmail.com>
2023-03-07 04:26:00 +0100

    Trim setup now that it's in schema; use find_or_create_by for test reruns

--- bullet_train-outgoing_webhooks/test/setup/active_record.rb
+++ bullet_train-outgoing_webhooks/test/setup/active_record.rb
@@ -1,40 +1,7 @@
-require "active_record"
-
-# Set so Active Record casts created_at/updated_at to ActiveSupport::TimeWithZone.
-# (Mathces Rails apps' default, but its assigned via Active Record's railtie.)
-Time.zone = "UTC"
-ActiveRecord::Base.time_zone_aware_attributes = true
-
-ActiveRecord::Schema.define do
-  create_table :teams, force: true do |t|
-    t.string :name, null: false
-    t.timestamps null: false
-  end
-
-  create_table :webhooks_outgoing_endpoints, force: true do |t|
-    t.references :team
-    t.string :url, null: false
-    t.string :name, null: false
-    t.integer :event_type_ids, array: true, default: []
-    t.timestamps null: false
-  end
-
-  create_table :webhooks_outgoing_deliveries, force: true do |t|
-    t.references :team
-    t.references :endpoint
-  end
-end
-
-class Team < ActiveRecord::Base
-  has_many :webhooks_outgoing_endpoints, class_name: "Webhooks::Outgoing::Endpoint"
-end
-
-class Webhooks::Outgoing::Delivery; end
-
 5.times do |n|
   Team.create! name: "Generic name #{n}"
 end
 
 5.times do |n|
-  Webhooks::Outgoing::Endpoint.create! team_id: Team.first.id, name: "Generic name #{n}", url: "http://example.com/webhook-#{n}"
+  Webhooks::Outgoing::Endpoint.find_or_create_by! team: Team.first, name: "Generic name #{n}", url: "http://example.com/webhook-#{n}"
 end
