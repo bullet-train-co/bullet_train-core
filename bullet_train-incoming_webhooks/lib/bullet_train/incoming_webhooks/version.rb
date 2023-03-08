d3bda317b8a6e41c80b68e9ac7ec1075fee284bb
Andrew Culver <andrew.culver@gmail.com>
2023-03-07 16:45:37 -0800

    1.2.15

--- bullet_train-incoming_webhooks/lib/bullet_train/incoming_webhooks/version.rb
+++ bullet_train-incoming_webhooks/lib/bullet_train/incoming_webhooks/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module IncomingWebhooks
-    VERSION = "1.2.14"
+    VERSION = "1.2.15"
   end
 end
--- bullet_train-super_scaffolding/lib/bullet_train/super_scaffolding/version.rb
+++ bullet_train-super_scaffolding/lib/bullet_train/super_scaffolding/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module SuperScaffolding
-    VERSION = "1.2.14"
+    VERSION = "1.2.15"
   end
 end
