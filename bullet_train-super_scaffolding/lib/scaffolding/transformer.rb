399e8b79ac94c8c463bd9a1f2f5b7ade4f87cfb1
Gabriel Zayas <g-zayas@hotmail.com>
2023-03-11 11:08:36 +0900

    Skip `OTHER_TARGETS` when it is `nil` (#167)

--- bullet_train-super_scaffolding/lib/scaffolding/transformer.rb
+++ bullet_train-super_scaffolding/lib/scaffolding/transformer.rb
@@ -192,7 +192,7 @@ class Scaffolding::Transformer
     # If the file exists in the application repository, we want to target it there.
     return file if File.exist?(file)
 
-    ENV["OTHER_TARGETS"].split(",").each do |possible_target|
+    ENV["OTHER_TARGETS"]&.split(",")&.each do |possible_target|
       candidate_path = "#{possible_target}/#{file}".gsub("//", "/")
       return candidate_path if File.exist?(candidate_path)
     end
