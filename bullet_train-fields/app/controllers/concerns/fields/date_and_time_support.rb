58cb0b6a1cb5494fadf62396ee30d3e48c350f2b
gazayas <g-zayas@hotmail.com>
2023-03-11 21:42:16 +0900

    Rely on standard Rails formatting for Date and DateTime objects

--- bullet_train-fields/app/controllers/concerns/fields/date_and_time_support.rb
+++ bullet_train-fields/app/controllers/concerns/fields/date_and_time_support.rb
@@ -7,7 +7,8 @@ module Fields::DateAndTimeSupport
     if strong_params.dig(attribute).present?
       time_zone = ActiveSupport::TimeZone.new(strong_params[time_zone_attribute] || current_team.time_zone)
       strong_params.delete(time_zone_attribute)
-      strong_params[attribute] = time_zone.strptime(strong_params[attribute], t("global.formats.date_and_time"))
+      # TODO: Ensure we're doing this correctly, and try to use Rails' formatting options.
+      strong_params[attribute] = time_zone.strptime(strong_params[attribute], "%m/%d/%Y %l:%M %p")
     end
   end
 end
--- bullet_train/app/helpers/account/dates_helper.rb
+++ bullet_train/app/helpers/account/dates_helper.rb
@@ -1,40 +1,24 @@
 module Account::DatesHelper
-  # e.g. October 11, 2018
-  def display_date(timestamp, custom_date_format = nil)
+  def display_date(timestamp, format: nil)
     return nil unless timestamp
-    if custom_date_format
-      local_time(timestamp).strftime(custom_date_format)
-    elsif local_time(timestamp).year == local_time(Time.now).year
-      local_time(timestamp).strftime("%B %-d")
-    else
-      local_time(timestamp).strftime("%B %-d, %Y")
-    end
+    timestamp = set_to_user_time_zone(timestamp)
+    formatted_timestamp(timestamp, format: format)
+  end
+  alias_method :display_date_and_time, :display_date
+
+  private
+
+  # If the format is a Symbol, we use the standard Rails formatting.
+  # https://guides.rubyonrails.org/i18n.html#adding-date-time-formats
+  def formatted_timestamp(timestamp, format: nil)
+    format ||= :default
+    format.is_a?(Symbol) ? l(timestamp, format: format) : timestamp.strftime(format)
   end
 
-  # e.g. October 11, 2018 at 4:22 PM
-  # e.g. Yesterday at 2:12 PM
-  # e.g. April 24 at 7:39 AM
-  def display_date_and_time(timestamp, custom_date_format = nil, custom_time_format = nil)
-    return nil unless timestamp
-
-    # today?
-    if local_time(timestamp).to_date == local_time(Time.now).to_date
-      "Today at #{display_time(timestamp, custom_time_format)}"
-    # yesterday?
-    elsif (local_time(timestamp).to_date) == (local_time(Time.now).to_date - 1.day)
-      "Yesterday at #{display_time(timestamp, custom_time_format)}"
-    else
-      "#{display_date(timestamp, custom_date_format)} at #{display_time(timestamp, custom_time_format)}"
-    end
-  end
-
-  # e.g. 4:22 PM
-  def display_time(timestamp, custom_time_format = nil)
-    local_time(timestamp).strftime(custom_time_format || "%l:%M %p")
-  end
-
-  def local_time(time)
-    return time if current_user.time_zone.nil?
-    time.in_time_zone(current_user.time_zone)
+  def set_to_user_time_zone(timestamp)
+    return timestamp if current_user.time_zone.nil?
+    is_date = timestamp.is_a?(Date)
+    timestamp = timestamp.in_time_zone(current_user.time_zone)
+    is_date ? timestamp.to_date : timestamp
   end
 end
