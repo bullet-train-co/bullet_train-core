240a643de08576f68f1a699e716e324557434d78
Gabriel Zayas <g-zayas@hotmail.com>
2023-03-14 18:00:35 +0900

    Refactor the dates helper

--- bullet_train/app/helpers/account/dates_helper.rb
+++ bullet_train/app/helpers/account/dates_helper.rb
@@ -1,24 +1,17 @@
 module Account::DatesHelper
-  def display_date(timestamp, format: nil)
-    return nil unless timestamp
-    timestamp = set_to_user_time_zone(timestamp)
-    formatted_timestamp(timestamp, format: format)
-  end
-  alias_method :display_date_and_time, :display_date
-
-  private
-
-  # If the format is a Symbol, we use the standard Rails formatting.
-  # https://guides.rubyonrails.org/i18n.html#adding-date-time-formats
-  def formatted_timestamp(timestamp, format: nil)
-    format ||= :default
-    format.is_a?(Symbol) ? l(timestamp, format: format) : timestamp.strftime(format)
+  def display_date(timestamp, format: :defaul)
+    localize(local_time(timestamp).to_date, format: format) if timestamp
   end
 
-  def set_to_user_time_zone(timestamp)
-    return timestamp if current_user.time_zone.nil?
-    is_date = timestamp.is_a?(Date)
-    timestamp = timestamp.in_time_zone(current_user.time_zone)
-    is_date ? timestamp.to_date : timestamp
+  def display_time(timestamp, format: :default)
+    localize(local_time(timestamp).to_time, format: format) if timestamp
+  end
+
+  def display_date_and_time(timestamp, format: :default)
+    localize(local_time(timestamp).to_datetime, format: format) if timestamp
+  end
+
+  def local_time(timestamp)
+    timestamp&.in_time_zone(current_user.time_zone || Time.zone)
   end
 end
