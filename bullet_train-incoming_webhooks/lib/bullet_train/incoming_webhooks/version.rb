f72fe13baf36d946289b93cea6d53777470e1c2d
Andrew Culver <andrew.culver@gmail.com>
2023-03-24 09:59:23 -0700

    1.2.22

--- bullet_train-incoming_webhooks/lib/bullet_train/incoming_webhooks/version.rb
+++ bullet_train-incoming_webhooks/lib/bullet_train/incoming_webhooks/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module IncomingWebhooks
-    VERSION = "1.2.21"
+    VERSION = "1.2.22"
   end
 end
--- bullet_train-integrations/lib/bullet_train/integrations/version.rb
+++ bullet_train-integrations/lib/bullet_train/integrations/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module Integrations
-    VERSION = "1.2.21"
+    VERSION = "1.2.22"
   end
 end
