61672eb1ea8984d7e98f1feb1d9824217c48b19f
Bhumi Shah <bhumi1102@gmail.com>
2023-03-31 09:40:33 -0500

    testing the purge job and cleanup

--- bullet_train-outgoing_webhooks/app/jobs/webhooks/outgoing/purge_job.rb
+++ bullet_train-outgoing_webhooks/app/jobs/webhooks/outgoing/purge_job.rb
@@ -5,15 +5,6 @@ class Webhooks::Outgoing::PurgeJob < ApplicationJob
     purge("Webhooks::Outgoing::Event", days_old)
     purge("Webhooks::Outgoing::Delivery", days_old)
     purge("Webhooks::Outgoing::DeliveryAttempt", days_old)
-    
-    # events = "Webhooks::Outgoing::Event".constantize.where("created_at < ?", days_old.days.ago)
-    # events.destroy_all
-
-    # deliveries = "Webhooks::Outgoing::Delivery".constantize.where("created_at < ?", days_old.days.ago)
-    # deliveries.destroy_all
-
-    # delivery_attempts = "Webhooks::Outgoing::DeliveryAttempt".constantize.where("created_at < ?", days_old.days.ago)
-    # delivery_attempts.destroy_all
   end
 
   def purge(table_name, days_old)
