486c8f4a8d6c66d0a680742f0e1594632c20b6e7
Andrew Culver <andrew.culver@gmail.com>
2023-04-02 21:44:44 -0700

    Update purge_job.rb

--- bullet_train-outgoing_webhooks/app/jobs/webhooks/outgoing/purge_job.rb
+++ bullet_train-outgoing_webhooks/app/jobs/webhooks/outgoing/purge_job.rb
@@ -2,13 +2,13 @@ class Webhooks::Outgoing::PurgeJob < ApplicationJob
   queue_as :default
 
   def perform(days_old = 90)
-    purge("Webhooks::Outgoing::Event", days_old)
-    purge("Webhooks::Outgoing::Delivery", days_old)
-    purge("Webhooks::Outgoing::DeliveryAttempt", days_old)
+    purge(Webhooks::Outgoing::Event, days_old)
+    purge(Webhooks::Outgoing::Delivery, days_old)
+    purge(Webhooks::Outgoing::DeliveryAttempt, days_old)
   end
 
-  def purge(table_name, days_old)
-    records = table_name.constantize.where("created_at < ?", days_old.days.ago)
+  def purge(model, days_old)
+    records = model.where("created_at < ?", days_old.days.ago)
     records.destroy_all
   end
 end
