1dd04e780b8727a884566a814cd1bf91fb7bd447
Gabriel Zayas <g-zayas@hotmail.com>
2023-06-05 21:33:17 +0900

    Install theme with Masamune

--- bullet_train-themes-light/lib/tasks/application.rb
+++ bullet_train-themes-light/lib/tasks/application.rb
@@ -1,3 +1,5 @@
+require "masamune"
+
 module BulletTrain
   module Themes
     module Application
@@ -148,26 +150,26 @@ module BulletTrain
       end
 
       def self.install_theme(theme_name)
-        # Grabs the current theme from
-        # def current_theme
-        #   :theme_name
-        # end
-        current_theme_regexp = /(^    :)(.*)/
         current_theme = nil
+        new_content = nil
+        File.open("./app/helpers/application_helper.rb", "r") do |f|
+          msmn = Masamune::AbstractSyntaxTree.new(f.readlines.join)
+          current_theme = msmn.symbols.first[:token]
+          new_content = msmn.replace(type: :symbols, old_token: current_theme, new_token: theme_name)
+        end
+
+        File.open("./app/helpers/application_helper.rb", "w") do |f|
+          f.puts new_content
+        end
 
         new_lines = []
         [
-          "./app/helpers/application_helper.rb",
           "./Procfile.dev",
           "./package.json"
         ].each do |file|
           File.open(file, "r") do |f|
             new_lines = f.readlines
             new_lines = new_lines.map do |line|
-              # Make sure we get the current theme before trying to replace it in any of the files.
-              # We grab it from the first file in the array above.
-              current_theme = line.scan(current_theme_regexp).flatten.last if line.match?(current_theme_regexp)
-
               line.gsub!(/#{current_theme}/, theme_name) unless current_theme.nil?
               line
             end
