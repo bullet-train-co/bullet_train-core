381559e3dec38fd6346c56ee80bb507e08a3eaf7
Kasper Timm Hansen <kaspth@gmail.com>
2023-07-10 17:39:16 +0200

    Remove unused `Billing::Usage::TeamSupport` include
    `TeamSupport` was renamed to `HasTrackers` in https://github.com/bullet-train-pro/bullet_train-billing-usage/commit/e4d3aedaae06d10d8700df747c0b2d79cfb2a3d8
    
    Additionally, the guide recommends adding `Billing::HasTrackers` (the guide needs updating to `Billing::Usage::HasTrackers` too) directly.
    
    So we aren't expecting this code to run and we can remove it.

--- bullet_train/app/models/concerns/teams/base.rb
+++ bullet_train/app/models/concerns/teams/base.rb
@@ -27,10 +27,6 @@ module Teams::Base
       if defined?(Billing::Stripe::Subscription)
         has_many :billing_stripe_subscriptions, class_name: "Billing::Stripe::Subscription", dependent: :destroy, foreign_key: :team_id
       end
-
-      if defined?(Billing::Usage::TeamSupport)
-        include Billing::Usage::TeamSupport
-      end
     end
 
     # validations
