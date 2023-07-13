6a9e03a767eb2b396b4d8b5853f3bb1fd709597f
Gabriel Zayas <g-zayas@hotmail.com>
2023-07-13 14:52:09 +0900

    Add account path correctly

--- bullet_train-super_scaffolding/lib/scaffolding/transformer.rb
+++ bullet_train-super_scaffolding/lib/scaffolding/transformer.rb
@@ -87,6 +87,7 @@ class Scaffolding::Transformer
       "Scaffolding/Completely Concrete/Tangible Things",
       "scaffolding/absolutely_abstract/creative_concepts",
       "scaffolding/completely_concrete/tangible_things",
+      "scaffolding/completely_concrete/_tangible_things",
       "scaffolding_absolutely_abstract_creative_concepts",
       "scaffolding_completely_concrete_tangible_things",
       "scaffolding-absolutely-abstract-creative-concepts",
@@ -94,7 +95,6 @@ class Scaffolding::Transformer
     ]
 
     class_name_with_context = [
-      # class name in context plural.
       "absolutely_abstract_creative_concepts",
       "completely_concrete_tangible_things",
       "absolutely_abstract/creative_concepts",
@@ -116,16 +116,18 @@ class Scaffolding::Transformer
       "tangible things",
     ]
 
-    full_class_exceptions = [
-      "scaffolding/completely_concrete/_tangible_things",
-      "scaffolding.completely_concrete.tangible_things",
-    ]
-
     (
-      full_class_name + full_class_exceptions + full_class_name.map(&:singularize) +
+      full_class_name +
+
+      # Address special cases here
+      full_class_name.map(&:singularize).reject {|str| str == "scaffolding/completely_concrete/_tangible_thing"} +
+      ["scaffolding.completely_concrete.tangible_things"] +
+
       class_name_with_context + class_name_with_context.map(&:singularize) +
       class_name + class_name.map(&:singularize) +
-      [":account", "/account"] # Account namespace vs. others.
+
+      # Account namespace vs. others.
+      [":account", "/account/"]
     ).each do |needle|
       string = string.gsub(needle, encode_double_replacement_fix(class_names_transformer.replacement_for(needle)))
     end
