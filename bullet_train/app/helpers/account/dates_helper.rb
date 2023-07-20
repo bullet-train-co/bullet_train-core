fb126f381d2e4e975aa647292614fedc184286e7
Samuel BOHN <bohn.sjl@gmail.com>
2023-07-20 22:30:57 +0200

    added current_time_zone and renamed tz_name_to_tz_id to time_zone_name_to_id

--- bullet_train/app/helpers/account/dates_helper.rb
+++ bullet_train/app/helpers/account/dates_helper.rb
@@ -42,7 +42,12 @@ module Account::DatesHelper
     !"#{I18n.t("time.am", fallback: false, default: "")}#{I18n.t("time.pm", fallback: false, default: "")}".empty?
   end
 
-  def tz_name_to_tz_id
+  def time_zone_name_to_id
     ActiveSupport::TimeZone.all.map{|tz| {"#{tz.name}" => tz.tzinfo.name}}.reduce({}, :merge)
+  end
+
+  def current_time_zone
+    current_time_zone_name = current_user&.time_zone || current_user&.current_team&.time_zone || "UTC"
+    ActiveSupport::TimeZone.find_tzinfo(current_time_zone_name).name
   end
 end
