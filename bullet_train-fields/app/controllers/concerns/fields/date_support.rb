69d11fa204a88cdb33139a4cd412d449cb5eb4ec
Jeremy Green <jeremy@octolabs.com>
2023-08-09 13:02:37 -0500

    Revert "default assign_date_and_time and assign_date to iso8601 parsing and fall back to legacy parsing"
    This reverts commit 4415e403e482bde970e5ae0cd8637692089fe214.

--- bullet_train-fields/app/controllers/concerns/fields/date_support.rb
+++ bullet_train-fields/app/controllers/concerns/fields/date_support.rb
@@ -10,13 +10,9 @@ module Fields::DateSupport
     )
     attribute = attribute.to_s
     if strong_params.dig(attribute).present?
-      begin
-        strong_params[attribute] = Date.iso8601(strong_params[attribute])
-      rescue ArgumentError
-        parsed_value = Chronic.parse(strong_params[attribute])
-        return nil unless parsed_value
-        strong_params[attribute] = parsed_value.to_date
-      end
+      parsed_value = Chronic.parse(strong_params[attribute])
+      return nil unless parsed_value
+      strong_params[attribute] = parsed_value.to_date
     end
   end
 end
