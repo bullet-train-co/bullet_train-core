eb03954a830d84fcf0cb113066d13ecde8c1d4e3
gazayas <g-zayas@hotmail.com>
2023-08-09 09:45:15 +0900

    Ensure custom date formats can be declared by both Rails defaults and Bullet Train internals

--- bullet_train/app/helpers/account/dates_helper.rb
+++ bullet_train/app/helpers/account/dates_helper.rb
@@ -1,13 +1,16 @@
 module Account::DatesHelper
-  def display_date(timestamp, format: :default)
+  def display_date(timestamp, format: :default, date_format: nil)
+    format = date_format if date_format
     localize(local_time(timestamp).to_date, format: format) if timestamp
   end
 
-  def display_time(timestamp, format: :default)
+  def display_time(timestamp, format: :default, time_format: nil)
+    format = time_format if time_format
     localize(local_time(timestamp).to_time, format: format) if timestamp
   end
 
-  def display_date_and_time(timestamp, format: :default)
+  def display_date_and_time(timestamp, format: :default, date_format: nil, time_format: nil)
+    format = "#{date_format} #{time_format}" if date_format && time_format
     localize(local_time(timestamp).to_datetime, format: format) if timestamp
   end
 
