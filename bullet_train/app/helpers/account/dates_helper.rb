cd3c0b1309d6ee80f63063d0549e0e778ed8d041
Jeremy Green <jeremy@octolabs.com>
2023-08-11 12:01:16 -0500

    preserve the previous call signatures

--- bullet_train/app/helpers/account/dates_helper.rb
+++ bullet_train/app/helpers/account/dates_helper.rb
@@ -1,15 +1,19 @@
 module Account::DatesHelper
-  def display_date(timestamp, format: :default, date_format: nil)
+  def display_date(timestamp, custom_date_format = nil, format: :default, date_format: nil)
+    date_format ||= custom_date_format
     format = date_format if date_format
     localize(local_time(timestamp).to_date, format: format) if timestamp
   end
 
-  def display_time(timestamp, format: :default, time_format: nil)
+  def display_time(timestamp, custom_time_format = nil, format: :default, time_format: nil)
+    time_format ||= custom_time_format
     format = time_format if time_format
     localize(local_time(timestamp).to_time, format: format) if timestamp
   end
 
-  def display_date_and_time(timestamp, format: :default, date_format: nil, time_format: nil)
+  def display_date_and_time(timestamp, custom_date_format = nil, custom_time_format = nil, format: :default, date_format: nil, time_format: nil)
+    date_format ||= custom_date_format
+    time_format ||= custom_time_format
     format = "#{date_format} #{time_format}" if date_format && time_format
     localize(local_time(timestamp).to_datetime, format: format) if timestamp
   end
