1ea5f7bc05477a839adad72b4ba5fb80e1c6b65c
gazayas <g-zayas@hotmail.com>
2023-08-19 17:53:47 +0900

    Set default values for InvitationList via controller

--- bullet_train/app/controllers/concerns/account/onboarding/invitation_lists/controller_base.rb
+++ bullet_train/app/controllers/concerns/account/onboarding/invitation_lists/controller_base.rb
@@ -20,6 +20,14 @@ module Account::Onboarding::InvitationLists::ControllerBase
 
   def create
     @account_onboarding_invitation_list = Account::Onboarding::InvitationList.create(account_onboarding_invitation_list_params)
+
+    # Set default values for invitations and memberships.
+    @account_onboarding_invitation_list.update(team: @team)
+    @account_onboarding_invitation_list.invitations.each do |invitation|
+      invitation.update(team: @team, from_membership: current_membership)
+      invitation.membership.update(team: @team)
+    end
+
     respond_to do |format|
       # We don't actually create a InvitationList record here, so we use `valid?` instead of `save`.
       if @account_onboarding_invitation_list.valid?
@@ -37,19 +45,16 @@ module Account::Onboarding::InvitationLists::ControllerBase
   # Never trust parameters from the scary internet, only allow the white list through.
   def account_onboarding_invitation_list_params
     params.require(:account_onboarding_invitation_list).permit(
-      :team_id,
-      :sender_id,
       invitations_attributes: [
-        :team_id,
-        :from_membership_id,
         :email,
         membership_attributes: [
-          :team_id,
-          :user_first_name,
-          :user_last_name,
           role_ids: []
         ]
       ]
     )
+  end
+
+  def current_membership
+    @user.memberships.find_by(team: @team)
   end
 end
