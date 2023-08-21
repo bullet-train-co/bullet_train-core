701b6869e710b2a2ac0e00dac8851e21138f2da9
Andrew Culver <andrew.culver@gmail.com>
2023-08-21 16:55:18 -0700

    Remove file deletion from incoming webhook scaffolder.

--- bullet_train-incoming_webhooks/lib/bullet_train/incoming_webhooks/scaffolders/incoming_webhooks_scaffolder.rb
+++ bullet_train-incoming_webhooks/lib/bullet_train/incoming_webhooks/scaffolders/incoming_webhooks_scaffolder.rb
@@ -21,7 +21,7 @@ module BulletTrain
           provider_name = argv.shift
           transformer = Scaffolding::IncomingWebhooksTransformer.new(provider_name)
 
-          `yes n | bin/rails g model webhooks_incoming_#{provider_name.tableize.singularize}_webhook data:jsonb processed_at:datetime verified_at:datetime`
+          `yes n | bin/rails g model Webhooks::Incoming::#{provider_name}Webhook data:jsonb processed_at:datetime verified_at:datetime`
 
           transformer.scaffold_incoming_webhook
 
