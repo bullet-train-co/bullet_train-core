0943f4a22d31eb526c2370e70dd8930ee4e2894c
Pascal Laliberté <pascal@hey.com>
2023-08-29 12:50:14 -0400

    moar declarative

--- bullet_train-fields/app/helpers/refresh_fields_helper.rb
+++ bullet_train-fields/app/helpers/refresh_fields_helper.rb
@@ -1,18 +1,30 @@
 module RefreshFieldsHelper
   def accept_query_string_override_for(form, method)
     field_name = form.field_name(method)
+
+    new_value = new_value_from_query_string(field_name)
+    return if new_value.nil?
+
+    form.object[method] = new_value
+  end
+
+  private
+
+  def new_value_from_query_string(field_name)
+    params.dig(*params_dig_path_for_field_name(field_name))
+  end
+
+  def params_dig_path_for_field_name(field_name)
+    dig_path = []
+
     nested_keys = Rack::Utils.parse_nested_query(field_name)
 
-    dig_path = []
     while !nested_keys.nil? && nested_keys.keys.size
       key = nested_keys.keys.first
       dig_path << key.to_sym
       nested_keys = nested_keys[key]
     end
 
-    new_value = params.dig(*dig_path)
-    return if new_value.nil?
-
-    form.object[method] = new_value
+    dig_path
   end
 end
