0b76066dbb2fa460d9467cc88162ce9d03d3d473
Gabriel Zayas <g-zayas@hotmail.com>
2023-08-31 20:09:00 +0900

    Wrap webhook test in scaffolding thing check

--- bullet_train-incoming_webhooks/test/controllers/webhooks/incoming/bullet_train_webhooks_controller_test.rb
+++ bullet_train-incoming_webhooks/test/controllers/webhooks/incoming/bullet_train_webhooks_controller_test.rb
@@ -3,42 +3,46 @@ require "test_helper"
 class Webhooks::Incoming::BulletTrainWebhooksControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
-  def setup
-    super
-    @user = FactoryBot.create(:onboarded_user)
-    sign_in @user
-    @team = @user.current_team
-  end
+  # We can only run this test when Scaffolding things are enabled
+  # because the default webhook triggers come from `CreativeConcept`, etc.
+  unless scaffolding_things_disabled?
+    def setup
+      super
+      @user = FactoryBot.create(:onboarded_user)
+      sign_in @user
+      @team = @user.current_team
+    end
 
-  test "should get incoming webhook" do
-    creative_concept = Scaffolding::AbsolutelyAbstract::CreativeConcept.create(name: "Test Concept")
+    test "should get incoming webhook" do
+      creative_concept = Scaffolding::AbsolutelyAbstract::CreativeConcept.create(name: "Test Concept")
 
-    webhook_params = {
-      data: {
+      webhook_params = {
         data: {
-          name: "Test",
-          team_id: {
-            id: @team.id,
-            slug: nil,
-            locale: nil,
-            time_zone: @team.time_zone,
-            created_at: creative_concept.created_at,
-            updated_at: creative_concept.updated_at,
-            being_destroyed: nil,
+          data: {
+            name: "Test",
+            team_id: {
+              id: @team.id,
+              slug: nil,
+              locale: nil,
+              time_zone: @team.time_zone,
+              created_at: creative_concept.created_at,
+              updated_at: creative_concept.updated_at,
+              being_destroyed: nil,
+            },
+            description: ""
           },
-          description: ""
+          event_type: "scaffolding/absolutely_abstract/creative_concept.created",
+          subject_type: "Scaffolding::AbsolutelyAbstract::CreativeConcept"
         },
-        event_type: "scaffolding/absolutely_abstract/creative_concept.created",
-        subject_type: "Scaffolding::AbsolutelyAbstract::CreativeConcept"
-      },
-      verified_at: nil,
-      processed_at: nil,
-    }
+        verified_at: nil,
+        processed_at: nil,
+      }
 
-    post "/webhooks/incoming/bullet_train_webhooks", params: webhook_params.to_json
-    assert_equal response.parsed_body, {"status" => "OK"}
+      post "/webhooks/incoming/bullet_train_webhooks", params: webhook_params.to_json
+      assert_equal response.parsed_body, {"status" => "OK"}
 
-    webhook = Webhooks::Incoming::BulletTrainWebhook.first
-    assert_equal webhook.data.to_json, webhook_params.to_json
+      webhook = Webhooks::Incoming::BulletTrainWebhook.first
+      assert_equal webhook.data.to_json, webhook_params.to_json
+    end
   end
 end
