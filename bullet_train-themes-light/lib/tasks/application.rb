70a1064236d63f247f690373425647835a09c843
Gabriel Zayas <g-zayas@hotmail.com>
2023-09-07 01:44:25 +0900

    Implement Masamune (#312)
    * Add Masamune to gemspec
    
    * Install theme with Masamune
    
    * Update Masamune to v1.1.4
    
    * Use Masamune to find namespace names
    
    * Fixing Standard Ruby
    
    * Edit msmn replacement code for v1.1.4
    
    * Use masamune v.1.1.5
    
    * Use more specific msmn method to find namespaces
    
    * Set msmn to attribute in RoutesFileManipulator constructor
    
    * Use Masamune v1.2.0
    
    * Refactor method_call logic in routes file manipulator
    
    * Use Pathname in install_theme, replace symbol in safer manner
    
    * Fixing Standard Ruby
    
    * Slight refactoring

--- bullet_train-themes-light/lib/tasks/application.rb
+++ bullet_train-themes-light/lib/tasks/application.rb
@@ -1,3 +1,5 @@
+require "masamune"
+
 module BulletTrain
   module Themes
     module Application
@@ -148,33 +150,16 @@ module BulletTrain
       end
 
       def self.install_theme(theme_name)
-        # Grabs the current theme from
-        # def current_theme
-        #   :theme_name
-        # end
-        current_theme_regexp = /(^    :)(.*)/
-        current_theme = nil
+        helper = Pathname.new("./app/helpers/application_helper.rb")
+        msmn = Masamune::AbstractSyntaxTree.new(helper.readlines.join)
+        current_theme_def = msmn.method_definitions(name: "current_theme").pop
+        current_theme = msmn.symbols.find { |node| node[:line_number] > current_theme_def[:line_number] }[:token]
+        helper.write msmn.replace(type: :symbol, old_token: current_theme, new_token: theme_name)
 
-        new_lines = []
-        [
-          "./app/helpers/application_helper.rb",
-          "./Procfile.dev",
-          "./package.json"
-        ].each do |file|
-          File.open(file, "r") do |f|
-            new_lines = f.readlines
-            new_lines = new_lines.map do |line|
-              # Make sure we get the current theme before trying to replace it in any of the files.
-              # We grab it from the first file in the array above.
-              current_theme = line.scan(current_theme_regexp).flatten.last if line.match?(current_theme_regexp)
-
-              line.gsub!(/#{current_theme}/, theme_name) unless current_theme.nil?
-              line
-            end
-          end
-
-          File.open(file, "w") do |f|
-            f.puts new_lines.join
+        [Pathname.new("./Procfile.dev"), Pathname.new("./package.json")].each do |file|
+          changed = file.read.gsub! current_theme, theme_name
+          if changed
+            file.write changed
           end
         end
       end
