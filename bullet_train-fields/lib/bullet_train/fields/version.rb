21bddcf961c06309fd643cdd37ad76a84d1183d1
jagthedrummer <jagthedrummer@users.noreply.github.com>
2023-09-20 19:03:34 +0000

    Version bump: patch - 1.4.6

--- bullet_train-fields/lib/bullet_train/fields/version.rb
+++ bullet_train-fields/lib/bullet_train/fields/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module Fields
-    VERSION = "1.4.5"
+    VERSION = "1.4.6"
   end
 end
--- bullet_train-themes-light/lib/bullet_train/themes/light/version.rb
+++ bullet_train-themes-light/lib/bullet_train/themes/light/version.rb
@@ -1,7 +1,7 @@
 module BulletTrain
   module Themes
     module Light
-      VERSION = "1.4.5"
+      VERSION = "1.4.6"
     end
   end
 end
