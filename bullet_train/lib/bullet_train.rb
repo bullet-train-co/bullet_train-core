764b9020452a23632dc499bb8d8181e9dab5d598
Jeremy Green <jeremy@octolabs.com>
2023-09-20 15:14:43 -0500

    another missing dependency

--- bullet_train/lib/bullet_train.rb
+++ bullet_train/lib/bullet_train.rb
@@ -3,6 +3,7 @@ require "bullet_train/engine"
 require "bullet_train/resolver"
 require "bullet_train/configuration"
 
+require "bullet_train/api"
 require "bullet_train/fields"
 require "bullet_train/roles"
 require "bullet_train/super_load_and_authorize_resource"
