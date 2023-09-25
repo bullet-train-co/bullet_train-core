12990bdd551ea8773e5923de3162845bff5f95ac
jagthedrummer <jagthedrummer@users.noreply.github.com>
2023-09-25 15:58:28 +0000

    Version bump: patch - 1.4.8

--- bullet_train-incoming_webhooks/lib/bullet_train/incoming_webhooks/version.rb
+++ bullet_train-incoming_webhooks/lib/bullet_train/incoming_webhooks/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module IncomingWebhooks
-    VERSION = "1.4.7"
+    VERSION = "1.4.8"
   end
 end
--- bullet_train-scope_validator/lib/bullet_train/scope_validator/version.rb
+++ bullet_train-scope_validator/lib/bullet_train/scope_validator/version.rb
@@ -2,6 +2,6 @@
 
 module BulletTrain
   module ScopeValidator
-    VERSION = "1.4.7"
+    VERSION = "1.4.8"
   end
 end
