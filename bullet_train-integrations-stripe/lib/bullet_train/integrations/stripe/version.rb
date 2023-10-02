94265196a02e723aca0d65977c46be10695ee047
jagthedrummer <jagthedrummer@users.noreply.github.com>
2023-10-02 21:14:32 +0000

    Version bump: patch - 1.5.1

--- bullet_train-integrations-stripe/lib/bullet_train/integrations/stripe/version.rb
+++ bullet_train-integrations-stripe/lib/bullet_train/integrations/stripe/version.rb
@@ -1,7 +1,7 @@
 module BulletTrain
   module Integrations
     module Stripe
-      VERSION = "1.5.0"
+      VERSION = "1.5.1"
     end
   end
 end
--- bullet_train-themes-light/lib/bullet_train/themes/light/version.rb
+++ bullet_train-themes-light/lib/bullet_train/themes/light/version.rb
@@ -1,7 +1,7 @@
 module BulletTrain
   module Themes
     module Light
-      VERSION = "1.5.0"
+      VERSION = "1.5.1"
     end
   end
 end
