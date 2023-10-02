4d7895d8b28f4bb918fdd5f787a637b29893e2d0
jagthedrummer <jagthedrummer@users.noreply.github.com>
2023-10-02 16:44:40 +0000

    Version bump: patch - 1.4.11

--- bullet_train-scope_validator/lib/bullet_train/scope_validator/version.rb
+++ bullet_train-scope_validator/lib/bullet_train/scope_validator/version.rb
@@ -2,6 +2,6 @@
 
 module BulletTrain
   module ScopeValidator
-    VERSION = "1.4.10"
+    VERSION = "1.4.11"
   end
 end
--- bullet_train-themes-tailwind_css/lib/bullet_train/themes/tailwind_css/version.rb
+++ bullet_train-themes-tailwind_css/lib/bullet_train/themes/tailwind_css/version.rb
@@ -1,7 +1,7 @@
 module BulletTrain
   module Themes
     module TailwindCss
-      VERSION = "1.4.10"
+      VERSION = "1.4.11"
     end
   end
 end
