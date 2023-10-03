ef0641f4ceb1ade832e14dc1f3220432146f7feb
Kasper Timm Hansen <kaspth@gmail.com>
2023-10-03 16:07:20 +0200

    Add a helper for the complex stdin mocking

--- bullet_train/test/lib/tasks/bullet_train_tasks_test.rb
+++ bullet_train/test/lib/tasks/bullet_train_tasks_test.rb
@@ -4,38 +4,39 @@ Rails.application.load_tasks
 
 module BulletTrain::Tasks; end
 class BulletTrain::Tasks::ResolveTaskTest < ActiveSupport::TestCase
-  setup { @original_stdin = $stdin }
-  teardown { $stdin = @original_stdin }
-
   test "resolves the path when passed BEGIN absolute path from annotated view" do
-    inputs = [
+    mock_stdin_with [
       "<!-- BEGIN /Users/andrewculver/.rbenv/versions/3.1.2/lib/ruby/gems/3.1.0/gems/bullet_train-themes-light-1.0.10/app/views/themes/light/workflow/_box.html.erb -->",
       "n", # Would you like to eject the file into the local project? (y/n):
       "n" # Would you like to open `#{source_file[:absolute_path]}`? (y/n):
-    ]
-    stdin_mock = StringIO.new(inputs.join("\n"))
-    stdin_mock.define_singleton_method(:ready?) { false }
-    $stdin = stdin_mock
-
-    assert_output /Absolute path:/ do
-      Rake::Task["bullet_train:resolve"].invoke("--interactive")
-      Rake::Task["bullet_train:resolve"].reenable
+    ] do
+      assert_output /Absolute path:/ do
+        Rake::Task["bullet_train:resolve"].invoke("--interactive")
+        Rake::Task["bullet_train:resolve"].reenable
+      end
     end
   end
 
   test "resolves the path when passed END absolute path from annotated view" do
-    inputs = [
+    mock_stdin_with [
       "<!-- END /Users/andrewculver/.rbenv/versions/3.1.2/lib/ruby/gems/3.1.0/gems/bullet_train-themes-light-1.0.10/app/views/themes/light/workflow/_box.html.erb -->",
       "n", # Would you like to eject the file into the local project? (y/n):
       "n" # Would you like to open `#{source_file[:absolute_path]}`? (y/n):
-    ]
-    stdin_mock = StringIO.new(inputs.join("\n"))
-    stdin_mock.define_singleton_method(:ready?) { false }
-    $stdin = stdin_mock
-
-    assert_output /Absolute path:/ do
-      Rake::Task["bullet_train:resolve"].invoke("--interactive")
-      Rake::Task["bullet_train:resolve"].reenable
+    ] do
+      assert_output /Absolute path:/ do
+        Rake::Task["bullet_train:resolve"].invoke("--interactive")
+        Rake::Task["bullet_train:resolve"].reenable
+      end
     end
+  end
+
+  private
+
+  def mock_stdin_with(inputs)
+    old_stdin, $stdin = $stdin, StringIO.new(inputs.join("\n"))
+    def $stdin.ready? = false # StringIO doens't have `IO#ready?`, so we stub a method on the singleton.
+    yield
+  ensure
+    $stdin = old_stdin
   end
 end
