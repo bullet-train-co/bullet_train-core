f81040123ec1e73c92fa8126659a4280943459b5
Pascal Laliberté <pascal@hey.com>
2023-10-04 15:21:20 -0400

    accept numerical new entries

--- bullet_train-fields/app/controllers/concerns/fields/super_select_support.rb
+++ bullet_train-fields/app/controllers/concerns/fields/super_select_support.rb
@@ -35,41 +35,21 @@ module Fields::SuperSelectSupport
     end
   end
 
-  def id_or_create_from_string(id_or_string)
-    return id_or_string unless id_or_string.present?
-
-    if /^\d+$/.match?(id_or_string)
-      id_or_string
-    else
-      yield(id_or_string).id.to_s
-    end
-  end
-
-  def ids_or_create_from_string(ids_or_strings)
-    ids_or_strings&.map do |id_or_string|
-      id_or_create_from_string(id_or_string) do
-        yield(id_or_string)
-      end
-    end
-  end
-
   def ensure_valid_id_or_create_model(id_or_string, collection: [], attribute: :id)
     return id_or_string unless id_or_string.present?
 
-    id = id_or_create_from_string(id_or_string) do |string|
-      yield(string, collection)
-    end
+    valid_id = collection.find_by(attribute => id_or_string)&.send(attribute)
+    return valid_id.to_s unless valid_id.nil?
 
-    collection.find_by(attribute => id).send(attribute)
+    yield(string, collection)&.id&.to_s
   end
 
   def ensure_valid_ids_or_create_model(ids_or_strings, collection: [], attribute: :id)
-    ids = ids_or_strings&.map do |id_or_string|
-      id_or_create_from_string(id_or_string) do |string|
-        yield(string, collection)
-      end
-    end
-    collection.valid_attribute_values([ids], attribute:)
-  end
+    valid_ids = collection.valid_attribute_values([ids], attribute:)
+    new_entries = ids_or_strings - valid_ids
 
+    valid_ids + new_entries.map { |string|
+      yield(string, collection)&.id&.to_s
+    }.compact_blank
+  end
 end
