7b2b7c9c1e9449e1a9b549f5d94f96052b9588dc
jagthedrummer <jagthedrummer@users.noreply.github.com>
2023-10-10 15:41:03 +0000

    Version bump: patch - 1.6.1

--- bullet_train-super_scaffolding/lib/bullet_train/super_scaffolding/version.rb
+++ bullet_train-super_scaffolding/lib/bullet_train/super_scaffolding/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module SuperScaffolding
-    VERSION = "1.6.0"
+    VERSION = "1.6.1"
   end
 end
--- bullet_train-themes-light/lib/bullet_train/themes/light/version.rb
+++ bullet_train-themes-light/lib/bullet_train/themes/light/version.rb
@@ -1,7 +1,7 @@
 module BulletTrain
   module Themes
     module Light
-      VERSION = "1.6.0"
+      VERSION = "1.6.1"
     end
   end
 end
