04a1c938c5c5682b2cbe8880da83dbbf228b4292
Jeremy Green <jeremy@octolabs.com>
2023-10-18 09:06:31 -0500

    Add more options to the super_scaffold generator

--- bullet_train-super_scaffolding/lib/generators/super_scaffold/super_scaffold_generator.rb
+++ bullet_train-super_scaffolding/lib/generators/super_scaffold/super_scaffold_generator.rb
@@ -43,7 +43,18 @@ class SuperScaffoldGenerator < Rails::Generators::Base
   argument :parent_models, type: :string
   argument :attributes, type: :array, default: [], banner: "attribute:type attribute:type"
 
-  class_option :skip_migration_generation, type: :boolean, default: false
+  class_option :skip_migration_generation, type: :boolean, default: false, desc: "Don't generate the model migration"
+  class_option :sortable, type: :boolean, default: false, desc: "https://bullettrain.co/docs/super-scaffolding/sortable"
+  class_option :namespace, type: :string, desc: "https://bullettrain.co/docs/namespacing"
+  class_option :sidebar, type: :string, desc: "Pass the Themify icon or Font Awesome icon to automatically add it to the navbar"
+  class_option :only_index, type: :boolean, default: false, desc: "Only scaffold the index view for a model"
+  class_option :skip_views, type: :boolean, default: false, desc: "Don't generate views"
+  class_option :skip_form, type: :boolean, default: false, desc: "Don't generate a new/edit form"
+  class_option :skip_locales, type: :boolean, default: false, desc: "Don't generate locale files"
+  class_option :skip_api, type: :boolean, default: false, desc: "Don't generate api files"
+  class_option :skip_model, type: :boolean, default: false, desc: "Don't generate a model file"
+  class_option :skip_controller, type: :boolean, default: false, desc: "Don't generate a controller file"
+  class_option :skip_routes, type: :boolean, default: false, desc: "Don't generate any routes"
 
   def generate
     # We add the name of the specific super_scaffolding command that we want to
