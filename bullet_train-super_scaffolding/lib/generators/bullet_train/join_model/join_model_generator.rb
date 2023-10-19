4ebd54f20f93607b1a1d7686a8b4885c5b86e0da
Jeremy Green <jeremy@octolabs.com>
2023-10-19 15:15:29 -0500

    moar options

--- bullet_train-super_scaffolding/lib/generators/bullet_train/join_model/join_model_generator.rb
+++ bullet_train-super_scaffolding/lib/generators/bullet_train/join_model/join_model_generator.rb
@@ -41,6 +41,16 @@ class BulletTrain::JoinModelGenerator < Rails::Generators::Base
   argument :left_association
   argument :right_association
 
+  class_option :skip_migration_generation, type: :boolean, default: false, desc: "Don't generate the model migration"
+  class_option :skip_form, type: :boolean, default: false, desc: "Don't alter the new/edit form"
+  class_option :skip_show, type: :boolean, default: false, desc: "Don't alter the show view"
+  class_option :skip_table, type: :boolean, default: false, desc: "Only add to the new/edit form and show view."
+  class_option :skip_locales, type: :boolean, default: false, desc: "Don't alter locale files"
+  class_option :skip_api, type: :boolean, default: false, desc: "Don't alter the api payloads"
+  class_option :skip_model, type: :boolean, default: false, desc: "Don't alter the model file"
+
+
+
   def generate
     # We add the name of the specific super_scaffolding command that we want to
     # invoke to the beginning of the argument string.
--- bullet_train-super_scaffolding/lib/generators/super_scaffold/field/field_generator.rb
+++ bullet_train-super_scaffolding/lib/generators/super_scaffold/field/field_generator.rb
@@ -42,6 +42,7 @@ class FieldGenerator < Rails::Generators::Base
   argument :model, type: :string
   argument :attributes, type: :array, default: [], banner: "attribute:type attribute:type"
 
+  class_option :skip_migration_generation, type: :boolean, default: false, desc: "Don't generate the model migration"
   class_option :skip_form, type: :boolean, default: false, desc: "Don't alter the new/edit form"
   class_option :skip_show, type: :boolean, default: false, desc: "Don't alter the show view"
   class_option :skip_table, type: :boolean, default: false, desc: "Only add to the new/edit form and show view."
