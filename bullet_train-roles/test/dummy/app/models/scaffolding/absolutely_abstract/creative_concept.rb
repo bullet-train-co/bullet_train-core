6bce2d01596295adc60281ca6afed28a8fe7ef33
Gabriel Zayas <g-zayas@hotmail.com>
2023-11-14 01:01:19 +0900

    Remove collaborators example (#653)
    * Remove collaborators example
    
    * Remove collaborator in other places
    
    * Remove collaborator-related fields from CreativeConcept show page
    
    * Remove collaborators logic from roles gem tests
    
    * Remove collaborator permission from dummy app
    
    * Add name heading back to Creative Concept index page

--- bullet_train-roles/test/dummy/app/models/scaffolding/absolutely_abstract/creative_concept.rb
+++ bullet_train-roles/test/dummy/app/models/scaffolding/absolutely_abstract/creative_concept.rb
@@ -3,9 +3,5 @@
 class Scaffolding::AbsolutelyAbstract::CreativeConcept < ApplicationRecord
   belongs_to :team
 
-  has_many :collaborators, class_name: "Scaffolding::AbsolutelyAbstract::CreativeConcepts::Collaborator", dependent: :destroy, foreign_key: :creative_concept_id
-
-  has_many :memberships, through: :collaborators
-
   validates :name, presence: true
 end
