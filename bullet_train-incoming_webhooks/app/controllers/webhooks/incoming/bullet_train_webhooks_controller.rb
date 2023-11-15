38deb9072509606372026e0981b4dcd07f922bf1
Jeremy Green <jeremy@octolabs.com>
2023-11-15 09:32:35 -0600

    trying to get tests passing

--- bullet_train-incoming_webhooks/app/controllers/webhooks/incoming/bullet_train_webhooks_controller.rb
+++ bullet_train-incoming_webhooks/app/controllers/webhooks/incoming/bullet_train_webhooks_controller.rb
@@ -1,6 +1,6 @@
 class Webhooks::Incoming::BulletTrainWebhooksController < Webhooks::Incoming::WebhooksController
   def create
-    Webhooks::Incoming::BulletTrainWebhook.create(data: JSON.parse(request.body.read)).process_async
+    Webhooks::Incoming::BulletTrainWebhook.create(data: JSON.parse(request.raw_post)).process_async
     render json: {status: "OK"}, status: :created
   end
 end
