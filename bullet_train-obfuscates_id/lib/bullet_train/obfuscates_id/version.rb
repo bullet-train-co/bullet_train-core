4a983677a8686a1228ead3081d85dc2fd0976e07
jagthedrummer <jagthedrummer@users.noreply.github.com>
2023-11-15 16:46:31 +0000

    Version bump: patch - 1.6.12

--- bullet_train-obfuscates_id/lib/bullet_train/obfuscates_id/version.rb
+++ bullet_train-obfuscates_id/lib/bullet_train/obfuscates_id/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module ObfuscatesId
-    VERSION = "1.6.11"
+    VERSION = "1.6.12"
   end
 end
--- bullet_train-themes/lib/bullet_train/themes/version.rb
+++ bullet_train-themes/lib/bullet_train/themes/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module Themes
-    VERSION = "1.6.11"
+    VERSION = "1.6.12"
   end
 end
