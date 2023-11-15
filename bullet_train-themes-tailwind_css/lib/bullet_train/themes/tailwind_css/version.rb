ecad75c97e4b037f4c61554f3ae2bcb1950c6e32
jagthedrummer <jagthedrummer@users.noreply.github.com>
2023-11-15 21:45:46 +0000

    Version bump: patch - 1.6.14

--- bullet_train-themes-tailwind_css/lib/bullet_train/themes/tailwind_css/version.rb
+++ bullet_train-themes-tailwind_css/lib/bullet_train/themes/tailwind_css/version.rb
@@ -1,7 +1,7 @@
 module BulletTrain
   module Themes
     module TailwindCss
-      VERSION = "1.6.13"
+      VERSION = "1.6.14"
     end
   end
 end
--- bullet_train/lib/bullet_train/version.rb
+++ bullet_train/lib/bullet_train/version.rb
@@ -1,3 +1,3 @@
 module BulletTrain
-  VERSION = "1.6.13"
+  VERSION = "1.6.14"
 end
