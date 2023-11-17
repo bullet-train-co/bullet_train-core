dd60d10590eb45c359410d2524d609231a825502
Julian Rubisch <julian@julianrubisch.at>
2023-11-17 17:15:42 +0100

    Don't use _commit callbacks when sending quantity updates (#677)
    * fix: Remove _commit callbacks
    
    * Update base.rb
    
    Co-authored-by: Jeremy Green <jeremy@octolabs.com>
    
    ---------
    
    Co-authored-by: Jeremy Green <jeremy@octolabs.com>

--- bullet_train/app/models/concerns/memberships/base.rb
+++ bullet_train/app/models/concerns/memberships/base.rb
@@ -30,9 +30,7 @@ module Memberships::Base
       end
     end
 
-    after_create_commit :publish_changed_quantity
-    after_update_commit :publish_changed_quantity
-    after_destroy_commit :publish_changed_quantity
+    after_commit :publish_changed_quantity
 
     after_validation :remove_user_profile_photo, if: :user_profile_photo_removal?
 
