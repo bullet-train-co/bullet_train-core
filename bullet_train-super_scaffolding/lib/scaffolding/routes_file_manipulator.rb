d3a4cc573f4b8c41b5e61cd601801c66ad19b1ea
Gabriel Zayas <g-zayas@hotmail.com>
2023-11-17 21:20:43 +0900

    Use Masamune for finding namespaces

--- bullet_train-super_scaffolding/lib/scaffolding/routes_file_manipulator.rb
+++ bullet_train-super_scaffolding/lib/scaffolding/routes_file_manipulator.rb
@@ -68,19 +68,20 @@ class Scaffolding::RoutesFileManipulator
   end
 
   def find_namespaces(namespaces, within = nil)
-    namespaces = namespaces.dup
     results = {}
-    block_end = Scaffolding::BlockManipulator.find_block_end(starting_from: within, lines: lines) if within
-    lines.each_with_index do |line, line_number|
-      if within
-        next unless line_number > within
-        return results if line_number >= block_end
-      end
-      if line.include?("namespace :#{namespaces.first} do")
-        results[namespaces.shift] = line_number
-      end
-      return results unless namespaces.any?
+    namespace_nodes = @msmn.method_calls(token_value: "namespace")
+    namespace_nodes.map do |node|
+      # Get the first argument (a Symbol) without the colon from the list of arguments.
+      name = node.arguments.child_nodes.first.unescaped
+      results[name] = node.line_number if namespaces.include?(name)
     end
+
+    # `within` uses an Array index whereas Masamune nodes use the actual line number, so we write `within + 1` here.
+    if within
+      block_end = @msmn.method_calls.find {|node| node.line_number == within + 1}.location.end_line
+      results.reject {|name, line_number| line_number >= block_end}
+    end
+
     results
   end
 
