b112856ccd033d5b580a3b9d0f765fcd93c51e71
jagthedrummer <jagthedrummer@users.noreply.github.com>
2023-11-17 18:13:30 +0000

    Version bump: patch - 1.6.16

--- bullet_train-api/lib/bullet_train/api/version.rb
+++ bullet_train-api/lib/bullet_train/api/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module Api
-    VERSION = "1.6.15"
+    VERSION = "1.6.16"
   end
 end
--- bullet_train-fields/lib/bullet_train/fields/version.rb
+++ bullet_train-fields/lib/bullet_train/fields/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module Fields
-    VERSION = "1.6.15"
+    VERSION = "1.6.16"
   end
 end
