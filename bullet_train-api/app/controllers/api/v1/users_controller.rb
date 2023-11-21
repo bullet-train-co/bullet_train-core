b696dc848cf0aa68bb8b1ce240277bea4c444095
Jeremy Green <jeremy@octolabs.com>
2023-11-21 10:29:33 -0600

    Make some notes on new 'default implementation' files

--- bullet_train-api/app/controllers/api/v1/users_controller.rb
+++ bullet_train-api/app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,6 @@
+# This is a default implementation of this file that we supply to help with gem tests.
+# The version from the starter repo will take precedence when running the full app.
+# You can think of the file in the starter repo as having been ejected from this gem.
 class Api::V1::UsersController < Api::V1::ApplicationController
   include Api::V1::Users::ControllerBase
 
--- bullet_train/app/controllers/application_controller.rb
+++ bullet_train/app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
+# This is a default implementation of this file that we supply to help with gem tests.
+# The version from the starter repo will take precedence when running the full app.
+# You can think of the file in the starter repo as having been ejected from this gem.
 class ApplicationController < ActionController::Base
   include Controllers::Base
 
