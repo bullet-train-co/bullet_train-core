97699635ea54826cc5f82a490c797eef289ee1e6
Jeremy Green <jeremy@octolabs.com>
2023-11-22 10:38:09 -0600

    try to get test reports on the summary page

--- bullet_train/test/test_helper.rb
+++ bullet_train/test/test_helper.rb
@@ -12,3 +12,26 @@ if ActiveSupport::TestCase.respond_to?(:fixture_path=)
   ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
   ActiveSupport::TestCase.fixtures :all
 end
+
+require "minitest/reporters"
+
+reporters = []
+
+if ENV["BT_TEST_FORMAT"]&.downcase == "dots"
+  # The classic "dot style" output:
+  # ...S..E...F...
+  reporters.push Minitest::Reporters::DefaultReporter.new
+else
+  # "Spec style" output that shows you which tests are executing as they run:
+  # UserTest
+  #   test_details_provided_should_be_true_when_details_are_provided  PASS (0.18s)
+  reporters.push Minitest::Reporters::SpecReporter.new(print_failure_summary: true)
+end
+
+# This reporter generates XML documents into test/reports that are used by CI services to tally results.
+# We add it last because doing so make the visible test output a little cleaner.
+reporters.push Minitest::Reporters::JUnitReporter.new if ENV["CI"]
+
+Minitest::Reporters.use! reporters
+
+
