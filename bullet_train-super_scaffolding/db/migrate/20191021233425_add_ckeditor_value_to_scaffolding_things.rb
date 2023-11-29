2d5f5f128c45beac697ce12271b020ec42ff052a
Gabriel Zayas <g-zayas@hotmail.com>
2023-11-29 20:48:21 +0900

    Remove bullet_train-super_scaffolding migrations

--- bullet_train-super_scaffolding/db/migrate/20191021233425_add_ckeditor_value_to_scaffolding_things.rb
+++
@@ -1,5 +0,0 @@
-class AddCkeditorValueToScaffoldingThings < ActiveRecord::Migration[6.0]
-  def change
-    add_column :scaffolding_things, :ckeditor_value, :text
-  end
-end
--- bullet_train-super_scaffolding/db/migrate/20191225224034_add_sort_order_to_scaffolding_completely_concrete_tangible_things.rb
+++
@@ -1,5 +0,0 @@
-class AddSortOrderToScaffoldingCompletelyConcreteTangibleThings < ActiveRecord::Migration[6.0]
-  def change
-    add_column :scaffolding_completely_concrete_tangible_things, :sort_order, :integer
-  end
-end
