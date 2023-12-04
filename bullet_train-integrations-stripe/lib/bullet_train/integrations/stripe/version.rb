61208e8614f5978a953eeb5d623e6a871a7f1b60
jagthedrummer <jagthedrummer@users.noreply.github.com>
2023-12-04 18:01:08 +0000

    Version bump: patch - 1.6.21

--- bullet_train-integrations-stripe/lib/bullet_train/integrations/stripe/version.rb
+++ bullet_train-integrations-stripe/lib/bullet_train/integrations/stripe/version.rb
@@ -1,7 +1,7 @@
 module BulletTrain
   module Integrations
     module Stripe
-      VERSION = "1.6.20"
+      VERSION = "1.6.21"
     end
   end
 end
--- bullet_train-super_scaffolding/lib/bullet_train/super_scaffolding/version.rb
+++ bullet_train-super_scaffolding/lib/bullet_train/super_scaffolding/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module SuperScaffolding
-    VERSION = "1.6.20"
+    VERSION = "1.6.21"
   end
 end
