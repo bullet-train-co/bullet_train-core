2f33965993096355053b692b25a5ce1ed0f63f3b
Kasper Timm Hansen <kaspth@gmail.com>
2023-12-07 11:59:42 +0100

    Fix standardrb

--- bullet_train-super_scaffolding/app/models/application_filter.rb
+++ bullet_train-super_scaffolding/app/models/application_filter.rb
@@ -30,7 +30,7 @@ class ApplicationFilter < Refine::Filter
     condition = CONDITIONS.fetch(type).new(field.to_s).with_display(heading(field))
 
     # Reject set/not_set clause options in case the field can't be null in the database.
-    if column = column_for(field)
+    if (column = column_for(field))
       condition = condition.without_clauses([Clauses::SET, Clauses::NOT_SET]) if column.null == false
     end
 
@@ -43,20 +43,19 @@ class ApplicationFilter < Refine::Filter
     model.connection.columns(model.table_name).index_by(&:name)[field.to_s]
   end
 
-
   def heading(field)
     t("#{field}.heading")
   end
 
   def options_for(field)
-    t("#{field}.options").map { { id: _1.to_s, display: _2 } }
+    t("#{field}.options").map { {id: _1.to_s, display: _2} }
   end
 
-  def t(key, **options)
-    I18n.t("#{i18n_scope}.fields.#{key}", **options)
+  def t(key, **)
+    I18n.t("#{i18n_scope}.fields.#{key}", **)
   end
 
   def i18n_scope
-    @i18n_scope ||= self.class.name.chomp("::Filter").pluralize.underscore.gsub("/", ".")
+    @i18n_scope ||= self.class.name.chomp("::Filter").pluralize.underscore.tr("/", ".")
   end
 end
