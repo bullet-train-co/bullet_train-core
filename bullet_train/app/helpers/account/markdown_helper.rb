6a7c3ee20c531fbe5abc242b087d9bc68624eee8
Pascal Laliberté <pascal@hey.com>
2024-01-08 16:51:16 -0500

    fix "<script" substitution to include "</script", clarify intent

--- bullet_train/app/helpers/account/markdown_helper.rb
+++ bullet_train/app/helpers/account/markdown_helper.rb
@@ -5,7 +5,9 @@ module Account::MarkdownHelper
         extensions: {header_ids: true},
         plugins: {syntax_highlighter: {theme: "InspiredGitHub"}},
         render: {width: 120, unsafe: true}
-      }).gsub("&lt;script", "<script").html_safe # force script tags to be rendered
+      }).gsub(/&lt;(\/)?script/,'<\\1script').html_safe
+      # force script tags to be rendered, for some reason unsafe: true doesn't ignore script tags
+      # only the first opening "<" needs re-replacing as the tailing ">" doesn't get replaced by the safety check
     else
       CommonMarker.render_html(string, :UNSAFE, [:table]).html_safe
     end
