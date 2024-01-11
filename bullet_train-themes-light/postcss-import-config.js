201ce9a96365861e7a5dea4a0c2302f2d79eade5
Pascal Laliberté <pascal@hey.com>
2024-01-11 13:38:55 -0500

    add extracted postcss-import-config.js

---
+++ bullet_train-themes-light/postcss-import-config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const { execSync } = require("child_process");
+
+const themeStylesheet = execSync(`bundle exec bin/theme tailwind-stylesheet ${process.env.THEME} 2> /dev/null`).toString().trim()
+const themeRoot = execSync(`bundle show bullet_train-themes-${process.env.THEME} 2> /dev/null`).toString().trim()
+const themeStylesheetsDir = path.resolve(themeRoot, 'app/assets/stylesheets/');
+
+module.exports = {
+  resolve: (id, basedir, importOptions) => {
+    if (id.startsWith('$ThemeStylesheetsDir')) {
+      initial_id = id;
+      id = id.replace('$ThemeStylesheetsDir', themeStylesheetsDir);
+    } else if (id.startsWith('$ThemeStylesheet')) {
+      id = id.replace('$ThemeStylesheet', themeStylesheet);
+    }
+    return id;
+  }
+}
