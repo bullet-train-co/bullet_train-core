40cbcaab6958fb5b4fbd3439a6f2382739dc3f86
jagthedrummer <jagthedrummer@users.noreply.github.com>
2024-01-22 16:47:47 +0000

    Version bump: patch - 1.6.28

--- bullet_train-api/lib/bullet_train/api/version.rb
+++ bullet_train-api/lib/bullet_train/api/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module Api
-    VERSION = "1.6.27"
+    VERSION = "1.6.28"
   end
 end
--- bullet_train-has_uuid/lib/bullet_train/has_uuid/version.rb
+++ bullet_train-has_uuid/lib/bullet_train/has_uuid/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module HasUuid
-    VERSION = "1.6.27"
+    VERSION = "1.6.28"
   end
 end
