7fbafef03ca508fc2768cea6d5ef04dfbe4cd542
Pascal Laliberté <pascal@hey.com>
2024-02-15 13:34:36 -0500

    defined linked_gems in bullet_train-themes if missing

--- bullet_train-themes/lib/bullet_train/themes/engine.rb
+++ bullet_train-themes/lib/bullet_train/themes/engine.rb
@@ -1,4 +1,8 @@
 module BulletTrain
+  unless respond_to?(:linked_gems)
+    mattr_accessor :linked_gems, default: ["bullet_train-themes"]
+  end
+
   module Themes
     class Engine < ::Rails::Engine
     end
