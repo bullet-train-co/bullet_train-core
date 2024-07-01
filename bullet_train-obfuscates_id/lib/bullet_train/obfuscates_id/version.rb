14b6c7fb4990fab23b44ea3e7bb4a0dd94487482
jagthedrummer <jagthedrummer@users.noreply.github.com>
2024-07-01 17:34:04 +0000

    Version bump: patch - 1.7.15

--- bullet_train-obfuscates_id/lib/bullet_train/obfuscates_id/version.rb
+++ bullet_train-obfuscates_id/lib/bullet_train/obfuscates_id/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module ObfuscatesId
-    VERSION = "1.7.14"
+    VERSION = "1.7.15"
   end
 end
--- bullet_train/lib/bullet_train/version.rb
+++ bullet_train/lib/bullet_train/version.rb
@@ -1,3 +1,3 @@
 module BulletTrain
-  VERSION = "1.7.14"
+  VERSION = "1.7.15"
 end
