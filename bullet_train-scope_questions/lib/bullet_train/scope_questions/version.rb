cb8da7d3128a82b94e66ce8aafd97ac5802f0252
jagthedrummer <jagthedrummer@users.noreply.github.com>
2024-07-09 16:25:08 +0000

    Version bump: patch - 1.7.16

--- bullet_train-scope_questions/lib/bullet_train/scope_questions/version.rb
+++ bullet_train-scope_questions/lib/bullet_train/scope_questions/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module ScopeQuestions
-    VERSION = "1.7.15"
+    VERSION = "1.7.16"
   end
 end
--- bullet_train-themes-tailwind_css/lib/bullet_train/themes/tailwind_css/version.rb
+++ bullet_train-themes-tailwind_css/lib/bullet_train/themes/tailwind_css/version.rb
@@ -1,7 +1,7 @@
 module BulletTrain
   module Themes
     module TailwindCss
-      VERSION = "1.7.15"
+      VERSION = "1.7.16"
     end
   end
 end
