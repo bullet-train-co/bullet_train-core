291e336ed2513f453cdc68fdf17dd2849aa345c0
jagthedrummer <jagthedrummer@users.noreply.github.com>
2024-08-20 18:54:00 +0000

    Version bump: patch - 1.7.21

--- bullet_train-incoming_webhooks/lib/bullet_train/incoming_webhooks/version.rb
+++ bullet_train-incoming_webhooks/lib/bullet_train/incoming_webhooks/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module IncomingWebhooks
-    VERSION = "1.7.20"
+    VERSION = "1.7.21"
   end
 end
--- bullet_train-integrations/lib/bullet_train/integrations/version.rb
+++ bullet_train-integrations/lib/bullet_train/integrations/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module Integrations
-    VERSION = "1.7.20"
+    VERSION = "1.7.21"
   end
 end
