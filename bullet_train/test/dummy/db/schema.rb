8628ae6f8e3f0edbb9821544200739654873d758
Jeremy Green <jeremy@octolabs.com>
2024-11-19 16:35:22 -0600

    A failing test for getting the time_zone from the first user

--- bullet_train/test/dummy/db/schema.rb
+++ bullet_train/test/dummy/db/schema.rb
@@ -22,12 +22,23 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_02_235728) do
     t.index ["membership_id"], name: "index_documents_on_membership_id"
   end
 
-  create_table "memberships", force: :cascade do |t|
-    t.jsonb "role_ids"
-    t.bigint "user_id", null: false
-    t.bigint "team_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "memberships", id: :serial, force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "team_id"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.bigint "invitation_id"
+    t.string "user_first_name"
+    t.string "user_last_name"
+    t.string "user_profile_photo_id"
+    t.string "user_email"
+    t.bigint "added_by_id"
+    t.bigint "platform_agent_of_id"
+    t.jsonb "role_ids", default: []
+    t.boolean "platform_agent", default: false
+    t.index ["added_by_id"], name: "index_memberships_on_added_by_id"
+    t.index ["invitation_id"], name: "index_memberships_on_invitation_id"
+    t.index ["platform_agent_of_id"], name: "index_memberships_on_platform_agent_of_id"
     t.index ["team_id"], name: "index_memberships_on_team_id"
     t.index ["user_id"], name: "index_memberships_on_user_id"
   end
@@ -61,14 +72,44 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_02_235728) do
     t.string "locale"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at", precision: nil
+    t.datetime "last_sign_in_at", precision: nil
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.integer "current_team_id"
     t.string "first_name"
     t.string "last_name"
-    t.integer "current_team_id"
+    t.string "time_zone"
+    t.datetime "last_seen_at", precision: nil
+    t.string "profile_photo_id"
     t.jsonb "ability_cache"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "last_notification_email_sent_at", precision: nil
+    t.boolean "former_user", default: false, null: false
+    t.string "encrypted_otp_secret"
+    t.string "encrypted_otp_secret_iv"
+    t.string "encrypted_otp_secret_salt"
+    t.integer "consumed_timestep"
+    t.boolean "otp_required_for_login"
+    t.string "otp_backup_codes", array: true
+    t.string "locale"
+    t.bigint "platform_agent_of_id"
+    t.string "otp_secret"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["platform_agent_of_id"], name: "index_users_on_platform_agent_of_id"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
   add_foreign_key "documents", "memberships"
--- bullet_train/test/models/team_test.rb
+++ bullet_train/test/models/team_test.rb
@@ -5,4 +5,12 @@ class TeamTest < ActiveSupport::TestCase
     team = Team.new
     assert_equal "UTC", team.time_zone
   end
+
+  test "a new team gets the time_zone of the first user when they join" do
+    team = Team.create!(name: "new test team")
+    user = User.create!(email: "test@test.com", password: "password", password_confirmation: "password", time_zone: "Central Time (US & Canada)")
+    Membership.create!(team: team, user: user)
+    team.reload
+    assert_equal "Central Time (US & Canada)", team.time_zone
+  end
 end
