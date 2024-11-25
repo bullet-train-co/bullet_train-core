8c0d4dde9e4c4ecc2ccfa8627776b3f698140fa7
Jeremy Green <jeremy@octolabs.com>
2024-11-25 11:57:55 -0600

    linter

--- bullet_train-outgoing_webhooks/app/controllers/api/v1/webhooks/outgoing/events_controller.rb
+++ bullet_train-outgoing_webhooks/app/controllers/api/v1/webhooks/outgoing/events_controller.rb
@@ -1,3 +1,3 @@
 class Api::V1::Webhooks::Outgoing::EventsController < Api::V1::ApplicationController
-   include Api::V1::Webhooks::Outgoing::Events::ControllerBase
+  include Api::V1::Webhooks::Outgoing::Events::ControllerBase
 end
