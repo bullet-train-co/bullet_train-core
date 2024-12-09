ff81e062db5af33279b381666ae1d0ad5ca5d372
Jeremy Green <jeremy@octolabs.com>
2024-12-09 11:25:54 -0600

    make a test fail

--- bullet_train/test/models/team_test.rb
+++ bullet_train/test/models/team_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class TeamTest < ActiveSupport::TestCase
   test "a new team defaults time_zone to UTC" do
     team = Team.new
+    team.wat
     assert_equal "UTC", team.time_zone
   end
 
