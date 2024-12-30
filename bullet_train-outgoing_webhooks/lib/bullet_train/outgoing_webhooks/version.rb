27c5267c06264a971f4fea11e246d206f55779fc
Jeremy Green <jeremy@octolabs.com>
2024-12-30 13:22:03 -0600

    Version bump: minor - 1.14.0 (#1003)
    Co-authored-by: jagthedrummer <jagthedrummer@users.noreply.github.com>

--- bullet_train-outgoing_webhooks/lib/bullet_train/outgoing_webhooks/version.rb
+++ bullet_train-outgoing_webhooks/lib/bullet_train/outgoing_webhooks/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module OutgoingWebhooks
-    VERSION = "1.13.0"
+    VERSION = "1.14.0"
   end
 end
--- bullet_train-scope_questions/lib/bullet_train/scope_questions/version.rb
+++ bullet_train-scope_questions/lib/bullet_train/scope_questions/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module ScopeQuestions
-    VERSION = "1.13.0"
+    VERSION = "1.14.0"
   end
 end
