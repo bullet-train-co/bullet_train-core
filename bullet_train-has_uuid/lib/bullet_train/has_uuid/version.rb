4f5eed4cec95886f5b9e124290500b1de4fca237
jagthedrummer <jagthedrummer@users.noreply.github.com>
2025-01-27 20:09:36 +0000

    Version bump: patch - 1.14.2

--- bullet_train-has_uuid/lib/bullet_train/has_uuid/version.rb
+++ bullet_train-has_uuid/lib/bullet_train/has_uuid/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module HasUuid
-    VERSION = "1.14.1"
+    VERSION = "1.14.2"
   end
 end
--- bullet_train-integrations-stripe/lib/bullet_train/integrations/stripe/version.rb
+++ bullet_train-integrations-stripe/lib/bullet_train/integrations/stripe/version.rb
@@ -1,7 +1,7 @@
 module BulletTrain
   module Integrations
     module Stripe
-      VERSION = "1.14.1"
+      VERSION = "1.14.2"
     end
   end
 end
