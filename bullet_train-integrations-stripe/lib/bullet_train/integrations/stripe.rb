6c6e534819bfb10ceb745bfe3e5b33a499acb94d
Jeremy Green <jeremy@octolabs.com>
2025-01-29 13:51:42 -0600

    Update stripe connect and omniauth gems (#1012)
    * Update omniauth and omniauth-stripe-connect-v2
    
    * we need this gem after all
    
    * Add some error logging when we hit an error
    
    * Commit a change to Gemfile.lock

--- bullet_train-integrations-stripe/lib/bullet_train/integrations/stripe.rb
+++ bullet_train-integrations-stripe/lib/bullet_train/integrations/stripe.rb
@@ -3,10 +3,14 @@ require "bullet_train/integrations/stripe/engine"
 
 require "stripe"
 require "omniauth"
-require "omniauth-stripe-connect"
+require "omniauth-stripe-connect-v2"
 
-# TODO Remove when we're able to properly upgrade Omniauth.
+# This helps to resolve this CVE:
 # https://github.com/omniauth/omniauth/wiki/Resolving-CVE-2015-9284
+# It also just allows things to work as expected.
+# Initially it seemed like we could remove this gem after updating omniauth
+# to version > 2. But If we remove it the built-in TokenValidator from omniauth
+# throws an error when we try to connect.
 require "omniauth/rails_csrf_protection"
 
 module BulletTrain
--- bullet_train/bullet_train.gemspec
+++ bullet_train/bullet_train.gemspec
@@ -43,7 +43,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency "colorizer"
   spec.add_dependency "devise"
   spec.add_dependency "xxhash"
-  spec.add_dependency "omniauth"
+  spec.add_dependency "omniauth", "~> 2.0"
 
   spec.add_dependency "image_processing"
 
