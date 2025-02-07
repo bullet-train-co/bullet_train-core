d3998105d37c3d6cedc1cc4659520d8a57a47fcb
Jeremy Green <jeremy@octolabs.com>
2025-02-07 13:11:15 -0600

    git rm -r bullet_train-scope_questions

--- bullet_train-scope_questions/lib/tasks/bullet_train/scope_questions_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :bullet_train_scope_questions do
-#   # Task goes here
-# end
--- bullet_train-scope_questions/test/bullet_train/scope_questions_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class BulletTrain::ScopeQuestionsTest < ActiveSupport::TestCase
-  test "it has a version number" do
-    assert BulletTrain::ScopeQuestions::VERSION
-  end
-end
