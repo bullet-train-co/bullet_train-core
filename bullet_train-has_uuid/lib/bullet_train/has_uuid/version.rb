d3dfbe02da5dcf5451c5cc8c0eecfdcf052f98d8
jagthedrummer <jagthedrummer@users.noreply.github.com>
2025-02-12 18:39:25 +0000

    Version bump: minor - 1.17.0

--- bullet_train-has_uuid/lib/bullet_train/has_uuid/version.rb
+++ bullet_train-has_uuid/lib/bullet_train/has_uuid/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module HasUuid
-    VERSION = "1.16.0"
+    VERSION = "1.17.0"
   end
 end
--- bullet_train-obfuscates_id/lib/bullet_train/obfuscates_id/version.rb
+++ bullet_train-obfuscates_id/lib/bullet_train/obfuscates_id/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module ObfuscatesId
-    VERSION = "1.16.0"
+    VERSION = "1.17.0"
   end
 end
