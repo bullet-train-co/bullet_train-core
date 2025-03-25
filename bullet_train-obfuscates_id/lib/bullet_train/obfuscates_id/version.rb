532509af297f421d18f448afc63a3269f901bdab
Jeremy Green <jeremy@octolabs.com>
2025-03-25 11:51:29 -0500

    Version bump: patch - 1.19.2 (#1079)
    Co-authored-by: jagthedrummer <jagthedrummer@users.noreply.github.com>

--- bullet_train-obfuscates_id/lib/bullet_train/obfuscates_id/version.rb
+++ bullet_train-obfuscates_id/lib/bullet_train/obfuscates_id/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module ObfuscatesId
-    VERSION = "1.19.1"
+    VERSION = "1.19.2"
   end
 end
--- bullet_train-themes-tailwind_css/lib/bullet_train/themes/tailwind_css/version.rb
+++ bullet_train-themes-tailwind_css/lib/bullet_train/themes/tailwind_css/version.rb
@@ -1,7 +1,7 @@
 module BulletTrain
   module Themes
     module TailwindCss
-      VERSION = "1.19.1"
+      VERSION = "1.19.2"
     end
   end
 end
