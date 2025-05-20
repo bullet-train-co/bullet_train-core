44a5391ceea0315dcf8d140d959528d29ef41242
jagthedrummer <jagthedrummer@users.noreply.github.com>
2025-05-20 20:13:02 +0000

    Version bump: minor - 1.20.0

--- bullet_train-themes-light/lib/bullet_train/themes/light/version.rb
+++ bullet_train-themes-light/lib/bullet_train/themes/light/version.rb
@@ -1,7 +1,7 @@
 module BulletTrain
   module Themes
     module Light
-      VERSION = "1.19.2"
+      VERSION = "1.20.0"
     end
   end
 end
--- bullet_train-themes-tailwind_css/lib/bullet_train/themes/tailwind_css/version.rb
+++ bullet_train-themes-tailwind_css/lib/bullet_train/themes/tailwind_css/version.rb
@@ -1,7 +1,7 @@
 module BulletTrain
   module Themes
     module TailwindCss
-      VERSION = "1.19.2"
+      VERSION = "1.20.0"
     end
   end
 end
