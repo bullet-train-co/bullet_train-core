dbcafe9c2bff47a5c9ea7e446b067c07b9d40b51
Sergey Andronov <sergey@andronov.me>
2025-05-26 12:00:18 +0200

    Add index for the deactivation migration

--- bullet_train-outgoing_webhooks/test/dummy/db/schema.rb
+++ bullet_train-outgoing_webhooks/test/dummy/db/schema.rb
@@ -94,6 +94,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_22_080219) do
     t.integer "api_version", null: false
     t.datetime "deactivation_limit_reached_at"
     t.datetime "deactivated_at"
+    t.index ["deactivated_at"], name: "index_webhooks_outgoing_endpoints_on_deactivated_at"
     t.index ["scaffolding_absolutely_abstract_creative_concept_id"], name: "index_endpoints_on_abstract_creative_concept_id"
     t.index ["team_id"], name: "index_webhooks_outgoing_endpoints_on_team_id"
   end
