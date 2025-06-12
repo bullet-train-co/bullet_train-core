83af7ca57dd2ca86a1fcb813a9866194025cd988
Sergey Andronov <sergey@andronov.me>
2025-06-12 16:43:39 +0200

    Extract create methods into helper

---
+++ bullet_train-outgoing_webhooks/test/setup/create_webhooks_test_helper.rb
@@ -0,0 +1,54 @@
+module CreateWebhooksTestHelpers
+  def create_user(options = {})
+    User.create!(
+      email: options[:email] || "test@example.com",
+      password: options[:password] || "password",
+    )
+  end
+
+  def create_endpoint(options = {})
+    Webhooks::Outgoing::Endpoint.create!(
+      url: options[:url] || "https://example.com/webhook",
+      name: options[:name] || "test",
+      team: options[:team] || @team,
+      event_type_ids: options[:event_type_ids] || ["fake-thing.create"],
+      deactivation_limit_reached_at: options[:deactivation_limit_reached_at] || nil,
+      deactivated_at: options[:deactivated_at] || nil,
+    )
+  end
+
+  def create_event(options = {})
+    Webhooks::Outgoing::Event.create!(
+      subject: options[:subject] || create_user,
+      event_type_id: options[:event_type_id] || "fake-thing.create",
+      team: options[:team] || @team,
+      payload: options[:payload] || {data: "test"},
+      api_version: options[:api_version] || 1,
+    )
+  end
+
+  def create_delivery(options = {})
+    Webhooks::Outgoing::Delivery.create!(
+      endpoint: options[:endpoint] || create_endpoint,
+      event: options[:event] || create_event,
+      endpoint_url: options[:endpoint_url] || "https://example.com/webhook",
+      delivered_at: options[:delivered_at] || nil,
+      created_at: options[:created_at] || Time.current
+    )
+  end
+
+  def create_delivery_attempts(delivery, count, options = {})
+    count.times do |i|
+      delivery.delivery_attempts.create!(
+        attempt_number: i + 1,
+        response_code: options[:response_code] || 500,
+        error_message: options[:error_message] || "Server error",
+        created_at: options[:created_at] || Time.current
+      )
+    end
+  end
+
+  def created_at_that_considered_failed
+    Webhooks::Outgoing::Delivery.max_attempts_period.ago - 1.hour
+  end
+end
--- bullet_train-outgoing_webhooks/test/test_helper.rb
+++ bullet_train-outgoing_webhooks/test/test_helper.rb
@@ -10,6 +10,7 @@ require "rails/test_help"
 
 require "setup/active_record"
 require "setup/endpoints"
+require "setup/create_webhooks_test_helper"
 
 # Load fixtures from the engine
 if ActiveSupport::TestCase.respond_to?(:fixture_path=)
