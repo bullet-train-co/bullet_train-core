c3ce9bc4719b6a21ce50d4832e042b7211bb6a79
Sergey Andronov <sergey@andronov.me>
2025-06-17 15:48:08 +0200

    Add tests for other secondary methods

--- bullet_train-outgoing_webhooks/test/models/concerns/webhooks/outgoing/endpoint_deactivatable_test.rb
+++ bullet_train-outgoing_webhooks/test/models/concerns/webhooks/outgoing/endpoint_deactivatable_test.rb
@@ -93,4 +93,140 @@ class Webhooks::Outgoing::EndpointDeactivatableTest < ActiveSupport::TestCase
     assert endpoint.marked_for_deactivation?
     assert notify_deactivation_limit_reached_called, "notify_deactivation_limit_reached should be called when endpoint is marked for deactivation"
   end
+
+  test "#deactivation_processing does nothing when neither deactivation condition is met" do
+    set_config(test_enabled_config)
+    endpoint = create_endpoint
+
+    assert_not endpoint.should_be_deactivated?
+    assert_not endpoint.should_be_marked_for_deactivation?
+
+    assert_no_changes -> { endpoint.deactivated_at } do
+      assert_no_changes -> { endpoint.deactivation_limit_reached_at } do
+        assert_changes -> { endpoint.consecutive_failed_deliveries }, from: 0, to: 1 do
+          endpoint.deactivation_processing
+        end
+      end
+    end
+  end
+
+  test "#should_be_deactivated? returns false when deactivation_limit_reached_at is nil" do
+    set_config(test_enabled_config)
+    endpoint = create_endpoint
+
+    assert_nil endpoint.deactivation_limit_reached_at
+    assert_not endpoint.should_be_deactivated?
+  end
+
+  test "#should_be_deactivated? returns false when already deactivated" do
+    set_config(test_enabled_config)
+    endpoint = create_endpoint(
+      deactivation_limit_reached_at: 2.days.ago,
+      deactivated_at: 1.hour.ago
+    )
+
+    assert endpoint.deactivated?
+    assert_not endpoint.should_be_deactivated?
+  end
+
+  test "#should_be_deactivated? returns false when deactivation_in period has not passed" do
+    set_config(test_enabled_config)
+    endpoint = create_endpoint(deactivation_limit_reached_at: 5.minutes.ago)
+
+    assert_not endpoint.should_be_deactivated?
+  end
+
+  test "#should_be_deactivated? returns true when deactivation_in period has passed" do
+    set_config(test_enabled_config)
+    endpoint = create_endpoint(deactivation_limit_reached_at: 4.days.ago)
+
+    assert endpoint.should_be_deactivated?
+  end
+
+  test "#should_be_marked_for_deactivation? returns false when endpoint is deactivated" do
+    set_config(test_enabled_config)
+    endpoint = create_endpoint(deactivated_at: 1.hour.ago)
+
+    assert endpoint.deactivated?
+    assert_not endpoint.should_be_marked_for_deactivation?
+  end
+
+  test "#should_be_marked_for_deactivation? returns false when already marked for deactivation" do
+    set_config(test_enabled_config)
+    endpoint = create_endpoint(deactivation_limit_reached_at: 1.hour.ago)
+
+    assert endpoint.marked_for_deactivation?
+    assert_not endpoint.should_be_marked_for_deactivation?
+  end
+
+  test "#should_be_marked_for_deactivation? returns false when consecutive_failed_deliveries is below threshold" do
+    set_config(test_enabled_config)
+    endpoint = create_endpoint(consecutive_failed_deliveries: 4)
+
+    assert_not endpoint.should_be_marked_for_deactivation?
+  end
+
+  test "#should_be_marked_for_deactivation? returns true when consecutive_failed_deliveries meets threshold" do
+    set_config(test_enabled_config)
+    endpoint = create_endpoint(consecutive_failed_deliveries: 5)
+
+    assert endpoint.should_be_marked_for_deactivation?
+  end
+
+  test "#should_be_marked_for_deactivation? returns true when consecutive_failed_deliveries much more than threshold" do
+    set_config(test_enabled_config)
+    endpoint = create_endpoint(consecutive_failed_deliveries: 100)
+
+    assert endpoint.should_be_marked_for_deactivation?
+  end
+
+  test "#mark_for_deactivation! sets deactivation_limit_reached_at" do
+    endpoint = create_endpoint(deactivation_limit_reached_at: nil)
+
+    freeze_time do
+      assert_changes -> { endpoint.deactivation_limit_reached_at }, from: nil, to: Time.current do
+        endpoint.mark_for_deactivation!
+      end
+    end
+  end
+
+  test "#mark_for_deactivation! does nothing when already marked for deactivation" do
+    endpoint = create_endpoint(deactivation_limit_reached_at: 1.hour.ago)
+
+    assert endpoint.marked_for_deactivation?
+
+    assert_no_changes -> { endpoint.deactivation_limit_reached_at } do
+      endpoint.mark_for_deactivation!
+    end
+  end
+
+  test "#mark_for_deactivation! does nothing when already deactivated" do
+    endpoint = create_endpoint(deactivated_at: 1.hour.ago)
+
+    assert endpoint.deactivated?
+
+    assert_no_changes -> { endpoint.deactivation_limit_reached_at } do
+      endpoint.mark_for_deactivation!
+    end
+  end
+
+  test "#deactivate! sets deactivated_at" do
+    endpoint = create_endpoint(deactivated_at: nil)
+
+    freeze_time do
+      assert_changes -> { endpoint.deactivated_at }, from: nil, to: Time.current do
+        endpoint.deactivate!
+      end
+    end
+  end
+
+  test "#deactivate! does nothing when already deactivated" do
+    endpoint = create_endpoint(deactivated_at: 1.hour.ago)
+
+    assert endpoint.deactivated?
+
+    assert_no_changes -> { endpoint.deactivated_at } do
+      endpoint.deactivate!
+    end
+  end
 end
