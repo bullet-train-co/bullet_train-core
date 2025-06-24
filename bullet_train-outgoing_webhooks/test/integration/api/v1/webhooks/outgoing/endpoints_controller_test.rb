486e9bdd57ad277f494e7633f49d41838af5bbb3
Sergey Andronov <sergey@andronov.me>
2025-06-24 14:05:34 +0200

    Allow to update only deactivated_at field for API

--- bullet_train-outgoing_webhooks/test/integration/api/v1/webhooks/outgoing/endpoints_controller_test.rb
+++ bullet_train-outgoing_webhooks/test/integration/api/v1/webhooks/outgoing/endpoints_controller_test.rb
@@ -56,25 +56,27 @@ class Api::V1::Webhooks::Outgoing::EndpointsControllerTest < ActionDispatch::Int
     assert_not @endpoint.deactivation_limit_reached_at
     assert_not @endpoint.deactivated_at
     assert_equal 0, @endpoint.consecutive_failed_deliveries
-    limit_reached_at = 3.days.ago.beginning_of_hour
     deactivated_at = Time.current.beginning_of_hour
 
     patch api_v1_webhooks_outgoing_endpoint_path(@endpoint.id), params: {
       webhooks_outgoing_endpoint: {
-        deactivation_limit_reached_at: limit_reached_at,
         deactivated_at: deactivated_at,
+        # restricted, added here just to check it was not updated
+        deactivation_limit_reached_at: Time.current,
         consecutive_failed_deliveries: 5
       }
     }, as: :json
 
     assert_response :success
     @endpoint.reload
-    assert_equal @endpoint.deactivation_limit_reached_at, limit_reached_at
-    assert_equal response.parsed_body["deactivation_limit_reached_at"], limit_reached_at.as_json
+    # updated
     assert_equal @endpoint.deactivated_at, deactivated_at
     assert_equal response.parsed_body["deactivated_at"], deactivated_at.as_json
-    assert_equal 5, @endpoint.consecutive_failed_deliveries
-    assert_equal 5, response.parsed_body["consecutive_failed_deliveries"]
+    # not updated
+    assert_not @endpoint.deactivation_limit_reached_at
+    assert_not response.parsed_body["deactivation_limit_reached_at"]
+    assert_equal 0, @endpoint.consecutive_failed_deliveries
+    assert_equal 0, response.parsed_body["consecutive_failed_deliveries"]
   end
 
   test "destroys an endpoint" do
