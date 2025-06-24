2e3efe689be4a63b453710e7e397cc283d3a99a5
Sergey Andronov <sergey@andronov.me>
2025-06-24 12:12:55 +0200

    Make index composite with team_id for the endpoint model

--- bullet_train-outgoing_webhooks/db/migrate/20250522080219_add_deactivation_fields_to_endpoints.rb
+++ bullet_train-outgoing_webhooks/db/migrate/20250522080219_add_deactivation_fields_to_endpoints.rb
@@ -4,6 +4,7 @@ class AddDeactivationFieldsToEndpoints < ActiveRecord::Migration[8.0]
   def change
     add_column :webhooks_outgoing_endpoints, :deactivation_limit_reached_at, :datetime
     add_column :webhooks_outgoing_endpoints, :deactivated_at, :datetime
-    add_index :webhooks_outgoing_endpoints, :deactivated_at, algorithm: :concurrently
+    parent_association = BulletTrain::OutgoingWebhooks.parent_association.to_s.foreign_key.to_sym
+    add_index :webhooks_outgoing_endpoints, [parent_association, :deactivated_at], algorithm: :concurrently
   end
 end
