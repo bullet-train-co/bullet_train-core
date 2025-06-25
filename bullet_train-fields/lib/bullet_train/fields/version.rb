4018c3b1ea9f1123dd0a271c151fdd561c5aed31
Jeremy Green <jeremy@octolabs.com>
2025-06-25 13:15:07 -0500

    Version bump: minor - 1.24.0 (#1163)
    Co-authored-by: jagthedrummer <jagthedrummer@users.noreply.github.com>

--- bullet_train-fields/lib/bullet_train/fields/version.rb
+++ bullet_train-fields/lib/bullet_train/fields/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module Fields
-    VERSION = "1.23.0"
+    VERSION = "1.24.0"
   end
 end
--- bullet_train-super_scaffolding/lib/bullet_train/super_scaffolding/version.rb
+++ bullet_train-super_scaffolding/lib/bullet_train/super_scaffolding/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module SuperScaffolding
-    VERSION = "1.23.0"
+    VERSION = "1.24.0"
   end
 end
