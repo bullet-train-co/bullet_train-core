92972a508022119a9f4ee484d6d082c7a6969dbb
jagthedrummer <jagthedrummer@users.noreply.github.com>
2025-07-31 20:09:30 +0000

    Version bump: patch - 1.26.1

--- bullet_train-fields/lib/bullet_train/fields/version.rb
+++ bullet_train-fields/lib/bullet_train/fields/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module Fields
-    VERSION = "1.26.0"
+    VERSION = "1.26.1"
   end
 end
--- bullet_train-themes-tailwind_css/lib/bullet_train/themes/tailwind_css/version.rb
+++ bullet_train-themes-tailwind_css/lib/bullet_train/themes/tailwind_css/version.rb
@@ -1,7 +1,7 @@
 module BulletTrain
   module Themes
     module TailwindCss
-      VERSION = "1.26.0"
+      VERSION = "1.26.1"
     end
   end
 end
