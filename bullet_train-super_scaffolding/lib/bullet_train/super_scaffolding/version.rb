4e9656c7f6599b80b86a814f1bf1af3564faf09a
jagthedrummer <jagthedrummer@users.noreply.github.com>
2025-08-19 17:45:26 +0000

    Version bump: minor - 1.27.0

--- bullet_train-super_scaffolding/lib/bullet_train/super_scaffolding/version.rb
+++ bullet_train-super_scaffolding/lib/bullet_train/super_scaffolding/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module SuperScaffolding
-    VERSION = "1.26.1"
+    VERSION = "1.27.0"
   end
 end
--- bullet_train/lib/bullet_train/version.rb
+++ bullet_train/lib/bullet_train/version.rb
@@ -1,3 +1,3 @@
 module BulletTrain
-  VERSION = "1.26.1"
+  VERSION = "1.27.0"
 end
