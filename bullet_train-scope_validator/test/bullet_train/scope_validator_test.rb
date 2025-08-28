99be983e87b01b880ab0c81ce7d5eff14930ae4e
Jeremy Green <jeremy@octolabs.com>
2025-08-28 13:02:23 -0500

    Move this test into the right place so that it runs

--- bullet_train-scope_validator/test/bullet_train/scope_validator_test.rb
+++ bullet_train-scope_validator/test/bullet_train/scope_validator_test.rb
@@ -2,12 +2,8 @@
 
 require "test_helper"
 
-class TestBulletTrain::ScopeValidator < Minitest::Test
+class BulletTrain::ScopeValidatorTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::BulletTrain::ScopeValidator::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
   end
 end
