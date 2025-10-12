9b27b4badb4df81f914932ec17508593fe3a5c2d
Pascal Laliberté <pascal@hey.com>
2025-10-12 17:38:08 -0400

    code_editor: updateComponentTheme

--- bullet_train-fields/app/javascript/controllers/fields/code_editor_controller.js
+++ bullet_train-fields/app/javascript/controllers/fields/code_editor_controller.js
@@ -142,7 +142,7 @@ export default class extends Controller {
     return window.monacoInstance
   }
 
-  updateTheme() {
+  updateComponentTheme() {
     this.codeEditor?.updateOptions({ theme: this.theme })
   }
 
