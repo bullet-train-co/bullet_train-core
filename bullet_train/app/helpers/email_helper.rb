4f9807eff30df46e730f6fe2620c051573354a1e
Yaro Shm <yshmarov@gmail.com>
2025-10-20 23:30:26 +0200

    email_image_tag should not duplicate attachments
    https://github.com/bullet-train-co/bullet_train-core/issues/1241#issuecomment-3422654266

--- bullet_train/app/helpers/email_helper.rb
+++ bullet_train/app/helpers/email_helper.rb
@@ -1,7 +1,9 @@
 module EmailHelper
   def email_image_tag(image, **)
     image_underscore = image.tr("-", "_")
-    attachments.inline[image_underscore] = File.read(Rails.root.join("app/assets/images/#{image}"))
+    unless attachments.inline[image_underscore]
+      attachments.inline[image_underscore] = File.read(Rails.root.join("app/assets/images/#{image}"))
+    end
     image_tag(attachments.inline[image_underscore].url, **)
   end
 end
