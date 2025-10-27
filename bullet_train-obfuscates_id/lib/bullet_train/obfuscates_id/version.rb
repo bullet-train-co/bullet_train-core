2b574fc2436a77c04ebe067a8f7e3c311be05ddf
jagthedrummer <jagthedrummer@users.noreply.github.com>
2025-10-27 18:43:23 +0000

    Version bump: minor - 1.32.0

--- bullet_train-obfuscates_id/lib/bullet_train/obfuscates_id/version.rb
+++ bullet_train-obfuscates_id/lib/bullet_train/obfuscates_id/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module ObfuscatesId
-    VERSION = "1.31.0"
+    VERSION = "1.32.0"
   end
 end
--- bullet_train-themes-tailwind_css/lib/bullet_train/themes/tailwind_css/version.rb
+++ bullet_train-themes-tailwind_css/lib/bullet_train/themes/tailwind_css/version.rb
@@ -1,7 +1,7 @@
 module BulletTrain
   module Themes
     module TailwindCss
-      VERSION = "1.31.0"
+      VERSION = "1.32.0"
     end
   end
 end
