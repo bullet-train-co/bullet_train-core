16a8ffbfcb4133f2ddc6ba778153fcde967b0e71
Jeremy Green <jeremy@octolabs.com>
2025-11-06 15:16:31 -0600

    Version bump: minor - 1.35.0 (#1265)
    Co-authored-by: jagthedrummer <jagthedrummer@users.noreply.github.com>

--- bullet_train-outgoing_webhooks/lib/bullet_train/outgoing_webhooks/version.rb
+++ bullet_train-outgoing_webhooks/lib/bullet_train/outgoing_webhooks/version.rb
@@ -1,5 +1,5 @@
 module BulletTrain
   module OutgoingWebhooks
-    VERSION = "1.34.1"
+    VERSION = "1.35.0"
   end
 end
--- bullet_train-themes-light/lib/bullet_train/themes/light/version.rb
+++ bullet_train-themes-light/lib/bullet_train/themes/light/version.rb
@@ -1,7 +1,7 @@
 module BulletTrain
   module Themes
     module Light
-      VERSION = "1.34.1"
+      VERSION = "1.35.0"
     end
   end
 end
