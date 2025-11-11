0da6f9bae9eb48c5aac569448ca9fb91b6c676b2
Jeremy Green <jeremy@octolabs.com>
2025-11-11 13:28:14 -0600

    Move it into the base model concern

--- bullet_train/app/models/concerns/invitations/base.rb
+++ bullet_train/app/models/concerns/invitations/base.rb
@@ -14,6 +14,8 @@ module Invitations::Base
     after_create :set_added_by_membership
     after_create :send_invitation_email
 
+    after_validation :hoist_membership_email_error
+
     attribute :uuid, default: -> { SecureRandom.hex }
 
     def roles
@@ -43,5 +45,16 @@ module Invitations::Base
 
   def is_for?(user)
     user.email.downcase.strip == email.downcase.strip
+  end
+
+  def hoist_membership_email_error
+    # This is special handling for the email field because we have a uniquness validation in the `Membership` model
+    # for the `user_email` field. Since we copy the value from `invitation.email` into `invitation.membership.user_email`
+    # the error isn't passed through to the form in the normal way.
+    if errors[:"membership.user_email"]
+      errors[:"membership.user_email"].each do |error|
+        errors.add(:email, error)
+      end
+    end
   end
 end
