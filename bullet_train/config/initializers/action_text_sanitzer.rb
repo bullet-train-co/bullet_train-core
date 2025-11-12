c1c876b1f74813ef13c8b1e8bcb12fa248e8c91c
Jeremy Green <jeremy@octolabs.com>
2025-11-12 15:37:18 -0600

    linter

--- bullet_train/config/initializers/action_text_sanitzer.rb
+++ bullet_train/config/initializers/action_text_sanitzer.rb
@@ -9,7 +9,7 @@ Rails.application.config.after_initialize do
   custom_allowed_attributes = Set.new(%w[controls])
   ActionText::ContentHelper.allowed_attributes = (default_allowed_attributes + custom_allowed_attributes).freeze
 
-  default_allowed_tags = Rails::HTML5::Sanitizer.safe_list_sanitizer.allowed_tags + Set.new([ ActionText::Attachment.tag_name, "figure", "figcaption" ])
+  default_allowed_tags = Rails::HTML5::Sanitizer.safe_list_sanitizer.allowed_tags + Set.new([ActionText::Attachment.tag_name, "figure", "figcaption"])
   custom_allowed_tags = Set.new(%w[audio video source])
   ActionText::ContentHelper.allowed_tags = (default_allowed_tags + custom_allowed_tags).freeze
 end
