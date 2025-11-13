9cad5b8fab6752298ac0ca3a43356948bae4bd69
Jeremy Green <jeremy@octolabs.com>
2025-11-13 12:43:27 -0600

    Render videos that are embeded in a TrixEditor (#1275)
    * Render videos that are embeded in a TrixEditor
    
    * add a comment about why this is so verbose
    
    * linter
    
    * linter again
    
    * Fix round trip editing for videos

---
+++ bullet_train/config/initializers/action_text_sanitzer.rb
@@ -0,0 +1,15 @@
+# This is to render previews for video attachments in Action Text
+# Source: https://mileswoodroffe.com/articles/action-text-video-support
+#
+# NOTE: This is all way more verbose that I'd like it to be. Maybe we can
+# slim it down if this Rails issue is ever fixed:
+# https://github.com/rails/rails/issues/54478
+Rails.application.config.after_initialize do
+  default_allowed_attributes = Rails::HTML5::Sanitizer.safe_list_sanitizer.allowed_attributes + ActionText::Attachment::ATTRIBUTES.to_set
+  custom_allowed_attributes = Set.new(%w[controls])
+  ActionText::ContentHelper.allowed_attributes = (default_allowed_attributes + custom_allowed_attributes).freeze
+
+  default_allowed_tags = Rails::HTML5::Sanitizer.safe_list_sanitizer.allowed_tags + Set.new([ActionText::Attachment.tag_name, "figure", "figcaption"])
+  custom_allowed_tags = Set.new(%w[audio video source])
+  ActionText::ContentHelper.allowed_tags = (default_allowed_tags + custom_allowed_tags).freeze
+end
