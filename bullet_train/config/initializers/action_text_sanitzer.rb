7b91085a6e259fac9fd3cd2d848758e1a85d3b31
Jeremy Green <jeremy@octolabs.com>
2025-11-13 14:00:46 -0600

    Set a poster for the video player
    This makes the player a little friendlier to users, and has the benefit
    of causing Rails to generate an image representation without us needing
    to include a hidden image on the page.

--- bullet_train/config/initializers/action_text_sanitzer.rb
+++ bullet_train/config/initializers/action_text_sanitzer.rb
@@ -6,7 +6,7 @@
 # https://github.com/rails/rails/issues/54478
 Rails.application.config.after_initialize do
   default_allowed_attributes = Rails::HTML5::Sanitizer.safe_list_sanitizer.allowed_attributes + ActionText::Attachment::ATTRIBUTES.to_set
-  custom_allowed_attributes = Set.new(%w[controls])
+  custom_allowed_attributes = Set.new(%w[controls poster type])
   ActionText::ContentHelper.allowed_attributes = (default_allowed_attributes + custom_allowed_attributes).freeze
 
   default_allowed_tags = Rails::HTML5::Sanitizer.safe_list_sanitizer.allowed_tags + Set.new([ActionText::Attachment.tag_name, "figure", "figcaption"])
